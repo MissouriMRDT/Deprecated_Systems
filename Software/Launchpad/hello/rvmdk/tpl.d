1a577b67d06f8b4a9a507f00fcc374519bb458f2
Keenan Johnson <keenan.johnson@gmail.com>
2014-01-17 14:02:04 -0600

    Adding base launchpad project that sends serial out

---
+++ Software/Launchpad/hello/rvmdk/tpl.d
@@ -0,0 +1,5 @@
+.\rvmdk\tpl.o: C:\Users\Zeus\Documents\GitHub\motherboard-squad\Software\tpl-master\src\tpl.c
+.\rvmdk\tpl.o: C:\Keil\ARM\ARMCC\bin\..\include\stdlib.h
+.\rvmdk\tpl.o: C:\Keil\ARM\ARMCC\bin\..\include\stdarg.h
+.\rvmdk\tpl.o: C:\Keil\ARM\ARMCC\bin\..\include\string.h
+.\rvmdk\tpl.o: C:\Keil\ARM\ARMCC\bin\..\include\stdio.h

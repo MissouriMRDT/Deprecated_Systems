5f765201210c1b7c356d7816beceddab017ddd0f
Keenan Johnson <keenan.johnson@gmail.com>
2014-02-14 17:41:52 -0600

    Aduino shunt now connects and reconnects properly

--- Software/ArduinoTCP_Proxy/Arduino_TCP_Proxy/Arduino_TCP_Proxy.ino
+++ Software/ArduinoTCP_Proxy/Arduino_TCP_Proxy/Arduino_TCP_Proxy.ino
@@ -5,10 +5,11 @@
 // The IP address will be dependent on your local network:
 byte mac[] = {  
   0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
-IPAddress ip(192,168,1,23);
+IPAddress ip(192,168,1,22);
 
 // Enter the IP address of the server you're connecting to:
 IPAddress server(192,168,1,2); 
+int port = 5000;
 
 // Initialize the Ethernet client library
 // with the IP address and port of the server 
@@ -29,15 +30,9 @@ void setup() {
   // give the Ethernet shield a second to initialize:
   delay(1000);
   Serial.println("connecting...");
-
-  // if you get a connection, report back via serial:
-  if (client.connect(server, 10002)) {
-    Serial.println("connected");
-  } 
-  else {
-    // if you didn't get a connection to the server:
-    Serial.println("connection failed");
-  }
+  
+  while(!client.connect(server, port));
+  Serial.println("connected!\n");
 }
 
 void loop()
@@ -60,14 +55,10 @@ void loop()
 
   // if the server's disconnected, stop the client:
   if (!client.connected()) {
-    Serial.println();
-    Serial.println("disconnecting.");
+    Serial.println("\ndisconnecting\n");
     client.stop();
-    // do nothing:
-    while(true);
+    Serial.println("Reconnecting...\n");
+    while(!client.connect(server, port));
   }
 }
-
-
-
 

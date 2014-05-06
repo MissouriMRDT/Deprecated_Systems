6bc05d25f84a52d5f9bda3aea19551094af851ba
Keenan Johnson <keenan.johnson@gmail.com>
2014-05-06 16:50:44 -0500

    Doing some cleanup

--- Software/CCS/main.c
+++ Software/CCS/main.c
@@ -36,15 +36,11 @@
 #include "include/muxes.h"
 #include "include/tcp.h"
 #include "include/uarts.h"
-#include "include/struct_test.h"
 #include "global.h"
 
 //Task Files
-//#include "tasks/tcp_task.h"
+#include "tasks/simple_drive.h"
 #include "tasks/Mux_test_task.h"
-#include "tasks/debug_console_task.h"
-#include "tasks/send_cmds.h"
-//#include "tasks/Struct_tcp_task.h"
 
 Queue_Handle debug_Q;
 

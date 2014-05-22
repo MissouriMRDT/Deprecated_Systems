5a7537df91313669a43def29ed83f2c51b468fb9
omc8db <omc8db@mst.edu>
2014-05-21 21:04:05 -0500

    Added revisions made after testing

--- sample_end_effector_software/ScienceBay/ScienceBay.ino
+++ sample_end_effector_software/ScienceBay/ScienceBay.ino
@@ -9,24 +9,23 @@
 #include <Servo.h>
 #include <EasyTransfer.h>
 
-const unsigned char SERVO_BIT = 0b00000001;
-const unsigned char LB395_BIT = 0b00000010;
-const unsigned char LB440_BIT = 0b00000100;
-
 struct control_struct
 {
-  unsigned char state; 
-  // takes form 0b00000abc
-  //Where A is status of 395nm lighting bank
-  //B is status of 440nm lighting bank
-  //C is status of servo
+  //1 for on, 0 for off
+  uint8_t lb395; 
+  
+  //1 for on, 0 for off
+  uint8_t lb440;
+  
+  //position. 255 for open, 0 for close
+  uint8_t doorserv;
 };
 
 const int OPEN = 180; 
 const int CLOSED = 0;
-const int SERVO_PIN = 9;
-const int LB395_PIN = 10;
-const int LB440_PIN = 11;
+const int SERVO_PIN = 11;
+const int LB395_PIN = 9;
+const int LB440_PIN = 10;
 Servo doorServo;
 char inchar;
 
@@ -36,23 +35,20 @@ EasyTransfer controls;
 void setup()
 {
   doorServo.attach(SERVO_PIN);
+  pinMode(LB395_PIN, OUTPUT);
+  pinMode(LB440_PIN, OUTPUT);
+  pinMode(SERVO_PIN, OUTPUT);
   controls.begin(details(control_data), &Serial);
   Serial.begin(9600);
 }
 
 void loop()
 {
-  //Open
-  while(Serial.available())
+  if(controls.receiveData())
   {
-    if(controls.receiveData())
-    {
-      digitalWrite(LB395_PIN, (control_data.state & LB395_BIT));
-      digitalWrite(LB440_PIN, (control_data.state & LB440_BIT));
-    }
+    digitalWrite(LB395_PIN, control_data.lb395);
+    digitalWrite(LB440_PIN, control_data.lb440);
   }
-  
-  //Opens servo if least significant bit of state is 1
-  //Closes door servo otherwise
-  doorServo.write(180*(control_data.state & SERVO_BIT));
+//  digitalWrite(LB440_PIN, LOW);
+  doorServo.write(control_data.doorserv);
 }

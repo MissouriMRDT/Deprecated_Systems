a1bd777d8cf2385f47807f0aac3ddf5205cdf4a6
omc8db <omc8db@mst.edu>
2014-05-24 18:38:32 -0500

    Initial test of cam control

--- cam_ctrl/cam_ctrl.ino
+++ cam_ctrl/cam_ctrl.ino
@@ -18,7 +18,13 @@ struct control_struct
   uint8_t roll;
   uint8_t mode;
 };
-
+  uint8_t lb395; 
+  
+  //1 for on, 0 for off
+  uint8_t lb440;
+  
+  //position. 255 for open, 0 for close
+  uint8_t doorserv;
 const int PITCH = 6; //Blue
 const int YAW = 9;   //Green
 const int ROLL = 10; //Red
@@ -48,9 +54,26 @@ void setup()
 
 void loop()
 {
+
   controls.receiveData();
   pitch.write(control_data.pitch);
   yaw.write(control_data.yaw);
   roll.write(control_data.roll);
   mode.write(control_data.mode);
+
+  /*Debug, please ignore
+  pitch.write(0);
+  yaw.write(0);
+  roll.write(0);
+  mode.write(0);
+  delay(500);
+  pitch.write(180);
+  delay(500);
+  yaw.write(180);
+  delay(500);
+  roll.write(180);
+  delay(500);
+  mode.write(180);
+  delay(500);
+  */
 }

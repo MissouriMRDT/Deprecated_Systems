594408a45dfb8c5b68379bafbcac6c2e388c383a
omc8db <omc8db@mst.edu>
2014-05-30 19:35:00 -0500

    Now supports reused gimble from last year's rover

--- cam_ctrl/cam_ctrl.ino
+++ cam_ctrl/cam_ctrl.ino
@@ -15,8 +15,8 @@ struct control_struct
 {
   uint8_t pitch;
   uint8_t yaw;
-  uint8_t roll;
-  uint8_t mode;
+//  uint8_t roll;
+//  uint8_t mode;
 };
 
 const int PITCH = 6; //Blue
@@ -25,8 +25,8 @@ const int ROLL = 10; //Red
 const int MODE = 11; //White
 Servo pitch;
 Servo yaw;
-Servo roll;
-Servo mode;
+//Servo roll;
+//Servo mode;
 
 
 control_struct control_data;
@@ -37,20 +37,20 @@ void setup()
   Serial.begin(115200);  
   pitch.attach(PITCH);
   yaw.attach(YAW);
-  roll.attach(ROLL);
-  mode.attach(MODE);
+  //roll.attach(ROLL);
+  //mode.attach(MODE);
   pinMode(PITCH, OUTPUT);
   pinMode(YAW, OUTPUT);
-  pinMode(ROLL, OUTPUT);
-  pinMode(MODE, OUTPUT);
+  //pinMode(ROLL, OUTPUT);
+  //pinMode(MODE, OUTPUT);
   control_data.pitch = 90;
   control_data.yaw = 90;
-  control_data.roll = 90;
-  control_data.mode = 90;
+  //control_data.roll = 90;
+  //control_data.mode = 90;
   controls.begin(details(control_data), &Serial);
   
   //Wait for camera module to initialize
-  delay(10000);
+  //delay(10000);
 }
 
 void loop()
@@ -59,8 +59,8 @@ void loop()
   controls.receiveData();
   pitch.write(control_data.pitch);
   yaw.write(control_data.yaw);
-  roll.write(control_data.roll);
-  mode.write(control_data.mode);
+  //roll.write(control_data.roll);
+  //mode.write(control_data.mode);
 
   
 }

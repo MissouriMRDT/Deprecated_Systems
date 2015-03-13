5ec4f92360cd5db1768f356b4212c83283b37538
Owen Chiaventone <omc8db@mst.edu>
2015-03-12 19:17:40 -0500

    Tester works in VM now

--- Software/Tests/NetworkTester/tester.py
+++ Software/Tests/NetworkTester/tester.py
@@ -1,49 +1,37 @@
-from tkinter import *
-from tkinter import ttk
+from Tkinter import *
+#from Tkinter import Ttk
 import socket
     
 def sendmessage(*args):
     global connection
     to_send = bytearray.fromhex(Data.get())
     connection.send(to_send)
-    print("Sent data ", to_send  )
+    print "Sent data ", to_send
 
 root = Tk()
 root.title("NetworkTestUtility")
 
-print("Waiting for Connection")
+print "Waiting for Connection"
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind((socket.gethostname(), 11000))
 server.listen(5)
 connection, addr = server.accept()
-print("Connected")
+print "Connected"
 
-mainframe = ttk.Frame(root, padding="3 3 12 12")
-mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
-mainframe.columnconfigure(0, weight=1)
-mainframe.rowconfigure(0, weight=1)
 
 Data = StringVar()
-meters = StringVar()
 
-data_entry = ttk.Entry(mainframe, width=24, textvariable = Data)
-data_entry.grid(column=2, row=1, sticky=(W, E))
-#feet_entry = ttk.Entry(mainframe, width=7, textvariable=feet)
-#feet_entry.grid(column=2, row=1, sticky=(W, E))
-
-ttk.Label(mainframe, textvariable=meters).grid(column=2, row=2, sticky=(W, E))
-ttk.Button(mainframe, text="Send", command=sendmessage).grid(column=3, row=3, sticky=W)
-
-ttk.Label(mainframe, text="Data").grid(column=3, row=1, sticky=W)
-
-for child in mainframe.winfo_children(): child.grid_configure(padx=5, pady=5)
+Label(root, text="Data: 0x").pack(side=TOP)
+data_entry = Entry(root, width=24, textvariable = Data)
+data_entry.pack(side=TOP)
+sendbutton = Button(root, text="Send", command=sendmessage).pack(side=TOP)
 
 data_entry.focus()
 root.bind('<Return>', sendmessage)
 try:
     root.mainloop()
 except:
-    print("Shit hit the fan")
+    print "Shit hit the fan"
     connection.close()
     server.close()
     quit()

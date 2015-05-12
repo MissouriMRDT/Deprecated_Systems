0931fffb7ee396a08ba46e8ac678c87b92e69a4c
Mike Miles <msa3204@mst.edu>
2015-05-11 22:35:09 -0500

    added file auxiliary_328_swser to allow comm with GPS via SW Serial
    Okay, so, with the previous copy of the GPS code (auxiliary_328), I had
    difficulty showing that I was getting the correct values via the
    Software Serial UARTS, however, Hardware Serial UARTS worked fine. The
    reason this is important is because our custom 32U4 boards do not
    currently have working Hardware Serial UARTS (we don't know why). It
    seems as though the problem was caused by a combination of (a) a lack of
    delays following Serial (SW and HW) begin functions and (b) the clock
    edits done in the previous version of Setup.h. Tl;dr auxiliary_328
    contains the code for communicating with GPS if we get Hardware Serial
    UARTS working on the custom 32U4 boards, auxiliary_328_swser contains
    the code for communicating with GPS if we must resort to using Software
    Serial UARTS. Now, because it is Finals Week, and I hate this code, I'm
    going home and going to bed. Goodnight!

---
+++ Software/sensor/auxiliary_328_swser/auxiliary_328_swser.ino
@@ -0,0 +1,126 @@
+#include <Adafruit_GPS.h> // Allows interaction with Adafruit GPS Module
+#include <SoftwareSerial.h> // Allows UART on non-hardware serial digital pins
+#include <EasyTransfer.h> // Simplifies transmission of structs
+#include "Setup.h"
+
+// create EasyTransfer object
+EasyTransfer ET;
+
+// create SoftwareSerial streams
+SoftwareSerial moboSerial(2, 3); // (Rx from mobo, Tx to mobo)
+SoftwareSerial gpsSerial(9, 10); // (Rx from GPS, Tx to GPS)
+
+// create Adafruit_GPS object
+Adafruit_GPS GPS(&gpsSerial);
+
+// Set GPSECHO to 'false' to turn off echoing the GPS data to the Serial console
+// Set to 'true' if you want to debug and listen to the raw GPS sentences
+#define GPSECHO false
+
+// Set TOSTRING to 'false' to turn off printing of parsed GPS data in GPS_Data struct to Serial console
+// Set to 'true' to print parsed data to Serial console
+#define TOSTRING true
+
+// Collect data from GPS every [data_delay] milliseconds
+#define data_delay 500
+
+// create GPS_Data struct to hold gps data
+GPS_Data gps_data;
+
+void setup()  
+{
+    
+  // connect at 115200 so we can read the GPS fast enough and echo without dropping chars also spit it out
+  Serial.begin(115200);
+  
+  // Start motherboard communication UART and EasyTransfer
+  moboSerial.begin(115200);
+  ET.begin(details(gps_data), &moboSerial);
+  delay(5000);
+  
+  // Start GPS parse object
+  GPS_setup(GPS, gps_data);
+  delay(1000);
+}
+
+uint32_t timer = millis();
+void loop()                     // run over and over again
+{
+  char c = GPS.read();
+  
+  // if true and valid, print raw gps data to serial monitor
+  if ((c) && (GPSECHO))
+    Serial.write(c); 
+         
+  // if a sentence is received, we can check the checksum, parse it...
+  if (GPS.newNMEAreceived()) {
+    // a tricky thing here is if we print the NMEA sentence, or data
+    // we end up not listening and catching other sentences! 
+    // so be very wary if using OUTPUT_ALLDATA and trying to print out data
+    //Serial.println(GPS.lastNMEA());   // this also sets the newNMEAreceived() flag to false
+  
+    if (!GPS.parse(GPS.lastNMEA()))   // this also sets the newNMEAreceived() flag to false
+      return;  // we can fail to parse a sentence in which case we should just wait for another
+  }
+
+  // if millis() or timer wraps around, we'll just reset it
+  if (timer > millis())  timer = millis();
+
+  // approximately every [data_delay] milliseconds or so, print out the current stats
+  if (millis() - timer > data_delay) { 
+    timer = millis(); // reset the timer
+    
+    gps_data.fix = GPS.fix;
+    if (GPS.fix){
+      gps_data.fixquality = GPS.fixquality;
+      gps_data.satellites = GPS.satellites;
+      gps_data.latitude_fixed = GPS.latitude_fixed;
+      gps_data.longitude_fixed = GPS.longitude_fixed;
+      gps_data.altitude = GPS.altitude;
+      gps_data.speed = GPS.speed;
+      gps_data.angle = GPS.angle;
+      
+      // Adjust latitude_fixed and longitude_fixed to reflect which hemispheres GPS is in (eliminates need for lat and lon)
+      if (GPS.lat == 'S') {
+        gps_data.latitude_fixed = -1 * gps_data.latitude_fixed;
+      }
+      if (GPS.lon == 'W') {
+        gps_data.longitude_fixed = -1 * gps_data.longitude_fixed;
+      }
+
+      // if true and valid, print parsed gps data to serial monitor 
+      if (TOSTRING)
+        gps_data.ToString();
+    
+    // Include time to know when last successful transmission was??
+//    Serial.print("\nTime: ");
+//    Serial.print(GPS.hour, DEC); Serial.print(':');
+//    Serial.print(GPS.minute, DEC); Serial.print(':');
+//    Serial.print(GPS.seconds, DEC); Serial.print('.');
+//    Serial.println(GPS.milliseconds);
+//    Serial.print("Date: ");
+//    Serial.print(GPS.day, DEC); Serial.print('/');
+//    Serial.print(GPS.month, DEC); Serial.print("/20");
+//    Serial.println(GPS.year, DEC);
+
+    
+//    Serial.print("Fix: "); Serial.print((int)GPS.fix);
+//    Serial.print(" quality: "); Serial.println((int)GPS.fixquality); 
+//    if (GPS.fix) {
+//      Serial.print("Location: ");
+//      Serial.print(GPS.latitude, 4); Serial.print(GPS.lat);
+//      Serial.print(", "); 
+//      Serial.print(GPS.longitude, 4); Serial.println(GPS.lon);
+//      
+//      Serial.print("Speed (knots): "); Serial.println(GPS.speed);
+//      Serial.print("Angle: "); Serial.println(GPS.angle);
+//      Serial.print("Altitude: "); Serial.println(GPS.altitude);
+//      Serial.print("Satellites: "); Serial.println((int)GPS.satellites);
+//    }
+
+     }
+    
+    // send data to motherboard via EasyTransfer
+//    ET.sendData();
+  }
+}

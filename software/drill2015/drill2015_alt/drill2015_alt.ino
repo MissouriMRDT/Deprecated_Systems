29f162b98c17f0455d1c051668500934bb679ec7
Mike Miles <msa3204@mst.edu>
2015-05-15 22:23:46 -0500

    updated drill code - tested w/ robo arm (not xbee), working on gripper

--- software/drill2015/drill2015_alt/drill2015_alt.ino
+++ software/drill2015/drill2015_alt/drill2015_alt.ino
@@ -32,22 +32,22 @@ const int pin_M1INB = 4;
 const int pin_M1PWM = 9;
 const int pin_M1CS = A0;
 
-SoftwareSerial ser(0, 1); // (Rx, Tx)
+SoftwareSerial moboSerial(0, 1); // (Rx, Tx)
 
-const char STOP = 0x30;
-const char FORW = 0x31;
-const char REV = 0x32;
+const char STOP = 0x00;
+const char FORW = 0x01;
+const char REV = 0x02;
 
-char cmd = STOP;
-bool cmd_M1EN = 0;
-bool cmd_M1INA = 0;
-bool cmd_M1INB = 0;
-bool cmd_M1PWM = 0;
+char motorCmd = STOP;
+bool motorCmd_M1EN = 0;
+bool motorCmd_M1INA = 0;
+bool motorCmd_M1INB = 0;
+bool motorCmd_M1PWM = 0;
 
 void setup() {
-  // serial with xbee
+  // Serial with xbee
   Serial.begin(9600);
-  ser.begin(9600);
+  moboSerial.begin(57600);
   
   // config motor driver select pins
   pinMode(pin_M1EN, OUTPUT);
@@ -59,45 +59,46 @@ void setup() {
 
 void loop() {
   // read in command from xbee
-  if(ser.available() > 0){
-    cmd = ser.read();
+  if(moboSerial.available() > 0){
+    motorCmd = moboSerial.read();
+    Serial.println(motorCmd, HEX);
   }
   
   // decide how to operate drill
-  if(cmd == FORW){ // drill forward
+  if(motorCmd == FORW){ // drill forward
     Serial.println("FORWARD");
-    cmd_M1EN = 1;
-    cmd_M1INA = 1;
-    cmd_M1INB = 0;
-    cmd_M1PWM = 1;
+    motorCmd_M1EN = 1;
+    motorCmd_M1INA = 1;
+    motorCmd_M1INB = 0;
+    motorCmd_M1PWM = 1;
     
   }
-  else if(cmd == REV){ // drill reverse
+  else if(motorCmd == REV){ // drill reverse
     Serial.println("REVERSE");
-    cmd_M1EN = 1;
-    cmd_M1INA = 0;
-    cmd_M1INB = 1;
-    cmd_M1PWM = 1;
+    motorCmd_M1EN = 1;
+    motorCmd_M1INA = 0;
+    motorCmd_M1INB = 1;
+    motorCmd_M1PWM = 1;
   }
-  else{
+  else{ // drill stop or bad transmission
     Serial.println("STOP");
-    cmd = STOP;
-    cmd_M1EN = 1;
-    cmd_M1INA = 0;
-    cmd_M1INB = 0; 
-    cmd_M1PWM = 0;
+    motorCmd = STOP;
+    motorCmd_M1EN = 1;
+    motorCmd_M1INA = 0;
+    motorCmd_M1INB = 0; 
+    motorCmd_M1PWM = 0;
   }
   
-  digitalWrite(pin_M1EN, cmd_M1EN);
-  digitalWrite(pin_M1INA, cmd_M1INA);
-  digitalWrite(pin_M1INB, cmd_M1INB);
-  digitalWrite(pin_M1PWM, cmd_M1PWM);
+  digitalWrite(pin_M1EN, motorCmd_M1EN);
+  digitalWrite(pin_M1INA, motorCmd_M1INA);
+  digitalWrite(pin_M1INB, motorCmd_M1INB);
+  digitalWrite(pin_M1PWM, motorCmd_M1PWM);
   
-//  Serial.print(cmd_INH); 
+//  Serial.print(motorCmd_INH); 
 //  Serial.print(" "); 
-//  Serial.print(cmd_IN1);
+//  Serial.print(motorCmd_IN1);
 //  Serial.print(" ");
-//  Serial.println(cmd_IN2);
+//  Serial.println(motorCmd_IN2);
   
   delay(20);
 }

897f33cd7721de6e2fecf5b15c0babd11c0adcf5
Owen Chiaventone <omc8db@mst.edu>
2015-05-17 17:48:04 -0500

    Generating 500kHz wave

--- Software/CCD_Reader/CCD_Reader.ino
+++ Software/CCD_Reader/CCD_Reader.ino
@@ -1,24 +1,71 @@
-#define CCD_CLOCK_LINE 4
+
+//Clock is on Port C, pin 26 (arduino pin 4)
+#define CLK_PORT             REG_PIOC_ODSR
+#define CLK_LINE             26
+#define CLK_LINE_ARDUINO     4
+
 #define CCD_SHUTTER 5
 #define CCD_ICG 6
 #define CCD_OS 0
 
+#include "DueTimer.h"
+
 //32-bit integer
-int clockCycle;
+volatile uint32_t clockCycle = 0;
+volatile bool bclock = false;
 
 const int CLOCKS_PER_CCD_CYCLE = 3;
 const int SIGNAL_ELEMENTS = 3648;
 const int PRE_DUMMY_ELEMENTS = 32;
 const int POST_DUMMY_ELEMENTS = 14;
 
-#include "DueTimer.h"
+//Number of clocks in a complete read cycle
+const uint32_t MAX_CLOCK = 1000000;
 
 uint32_t pwmPin = 8;
 uint32_t maxDutyCount = 2;
-uint32_t clkAFreq = 1000ul;
-uint32_t pwmFreq = 1000ul; 
+
+//Set up clocks for 1 Mhz
+uint32_t clkAFreq = 42000000ul;
+uint32_t pwmFreq = 1000000ul; 
+
+
+void fastDigitalWrite();
+void setUpClocks();
+
+
+void ccd_clock_ISR()
+{
+//  clockCycle ++;
+//  clockCycle %= MAX_CLOCK;
+  
+  //Flip the clock line with a bitwise XOR
+  CLK_PORT ^= 1 << CLK_LINE;
+}
  
 void setup() {
+  
+  pinMode(CLK_LINE_ARDUINO, OUTPUT);
+//  setUpClocks();
+  Timer1.attachInterrupt(ccd_clock_ISR);
+  
+  //Call every microsecond
+  Timer1.setFrequency(1000000);
+  Timer1.start();
+
+  
+  Serial.begin(115200);
+}
+ 
+void loop() 
+{
+  Serial.print("Time: ");
+  Serial.println(clockCycle, DEC);
+  delay(300);
+}
+
+void setUpClocks()
+{
   pmc_enable_periph_clk(PWM_INTERFACE_ID);
   PWMC_ConfigureClocks(clkAFreq, 0, VARIANT_MCK);
  
@@ -35,8 +82,4 @@ void setup() {
   PWMC_SetDutyCycle(PWM_INTERFACE, channel, 1);
  
   pmc_mck_set_prescaler(2);
-}
- 
-void loop() 
-{
 }
---
+++ Software/CCD_Reader/CcdReader.h
@@ -0,0 +1,9 @@
+#ifndef CCD_READER_H
+#define CCD_READER_H
+
+class CcdReader
+{
+  
+};
+
+#endif

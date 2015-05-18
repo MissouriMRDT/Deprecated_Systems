b14e28a68bb612300386a3838d9c196684d09a94
Owen Chiaventone <omc8db@mst.edu>
2015-05-17 20:36:49 -0500

    Generating 800kHz square wave w/ PWM generator

--- Software/CCD_Reader/CCD_Reader.ino
+++ Software/CCD_Reader/CCD_Reader.ino
@@ -1,85 +1,44 @@
+#include <Arduino.h>
 
-//Clock is on Port C, pin 26 (arduino pin 4)
-#define CLK_PORT             REG_PIOC_ODSR
-#define CLK_LINE             26
-#define CLK_LINE_ARDUINO     4
+#define CLOCK_PIN        8
+const int ICG =         4;
+const int SH =          5;
+const int OS =          0; //Analog
 
-#define CCD_SHUTTER 5
-#define CCD_ICG 6
-#define CCD_OS 0
+#define CLOCK_FREQUENCY 800000 //800 kHz
+#define CLOCK_MAX_DUTY  2       // 0 = off, 1 = 50%, 2 = on
 
-#include "DueTimer.h"
-
-//32-bit integer
-volatile uint32_t clockCycle = 0;
-volatile bool bclock = false;
-
-const int CLOCKS_PER_CCD_CYCLE = 3;
-const int SIGNAL_ELEMENTS = 3648;
-const int PRE_DUMMY_ELEMENTS = 32;
-const int POST_DUMMY_ELEMENTS = 14;
-
-//Number of clocks in a complete read cycle
-const uint32_t MAX_CLOCK = 1000000;
-
-uint32_t pwmPin = 8;
-uint32_t maxDutyCount = 2;
-
-//Set up clocks for 1 Mhz
-uint32_t clkAFreq = 42000000ul;
-uint32_t pwmFreq = 1000000ul; 
-
-
-void fastDigitalWrite();
 void setUpClocks();
-
-
-void ccd_clock_ISR()
-{
-//  clockCycle ++;
-//  clockCycle %= MAX_CLOCK;
-  
-  //Flip the clock line with a bitwise XOR
-  CLK_PORT ^= 1 << CLK_LINE;
-}
  
 void setup() {
   
-  pinMode(CLK_LINE_ARDUINO, OUTPUT);
-//  setUpClocks();
-  Timer1.attachInterrupt(ccd_clock_ISR);
-  
-  //Call every microsecond
-  Timer1.setFrequency(1000000);
-  Timer1.start();
-
-  
+  setUpClocks();
   Serial.begin(115200);
 }
  
 void loop() 
 {
-  Serial.print("Time: ");
-  Serial.println(clockCycle, DEC);
-  delay(300);
+
 }
 
 void setUpClocks()
 {
+  //Enable PWM controller peripheral
   pmc_enable_periph_clk(PWM_INTERFACE_ID);
-  PWMC_ConfigureClocks(clkAFreq, 0, VARIANT_MCK);
+  PWMC_ConfigureClocks(CLOCK_FREQUENCY * CLOCK_MAX_DUTY, 0, VARIANT_MCK);
  
   PIO_Configure(
-    g_APinDescription[pwmPin].pPort,
-    g_APinDescription[pwmPin].ulPinType,
-    g_APinDescription[pwmPin].ulPin,
-    g_APinDescription[pwmPin].ulPinConfiguration);
+    g_APinDescription[CLOCK_PIN].pPort,
+    g_APinDescription[CLOCK_PIN].ulPinType,
+    g_APinDescription[CLOCK_PIN].ulPin,
+    g_APinDescription[CLOCK_PIN].ulPinConfiguration);
  
-  uint32_t channel = g_APinDescription[pwmPin].ulPWMChannel;
-  PWMC_ConfigureChannel(PWM_INTERFACE, channel , pwmFreq, 0, 0);
-  PWMC_SetPeriod(PWM_INTERFACE, channel, maxDutyCount);
+  uint32_t channel = g_APinDescription[CLOCK_PIN].ulPWMChannel;
+  
+  PWMC_ConfigureChannel(PWM_INTERFACE, channel , PWM_CMR_CPRE_CLKA, 0, 0);
+  PWMC_SetPeriod(PWM_INTERFACE, channel, CLOCK_MAX_DUTY); //1 uS pulses
   PWMC_EnableChannel(PWM_INTERFACE, channel);
   PWMC_SetDutyCycle(PWM_INTERFACE, channel, 1);
  
-  pmc_mck_set_prescaler(2);
+  //pmc_mck_set_prescaler(2);
 }
--- Software/CCD_Reader/CcdReader.h
+++ Software/CCD_Reader/CcdReader.h
@@ -1,8 +1,16 @@
 #ifndef CCD_READER_H
 #define CCD_READER_H
 
+#include <stdint.h>
+
+const int CCD_SIZE = 3684
+
 class CcdReader
 {
+  CcdReader();
+  
+  uint16_t Data[CCD_SIZE]
+private:
   
 };
 

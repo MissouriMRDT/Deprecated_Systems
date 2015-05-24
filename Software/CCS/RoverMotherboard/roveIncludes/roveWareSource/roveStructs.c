5cd387eb24f9512c3f86282e208e46e0179a5cd8
Owen Chiaventone <omc8db@mst.edu>
2015-05-23 20:24:14 -0500

    Remove unnecessary cast

--- Software/CCS/RoverMotherboard/roveIncludes/roveWareSource/roveStructs.c
+++ Software/CCS/RoverMotherboard/roveIncludes/roveWareSource/roveStructs.c
@@ -12,10 +12,10 @@ int getStructSize(char structId) {
 
     switch (structId) {
 
-    case (char) motor_left_id:
+    case motor_left_id:
         return sizeof(struct motor_control_struct);
 
-    case (char) motor_right_id:
+    case motor_right_id:
         return sizeof(struct motor_control_struct);
 
     case bms_emergency_stop_command_id:
@@ -31,13 +31,13 @@ int getStructSize(char structId) {
             ... power_board_telem_main_battery_voltage_id:
         return sizeof(struct power_board_bms_telem);
 */
-    case (char) wrist_clock_wise ... drill_forward:
+    case wrist_clock_wise ... drill_forward:
         return sizeof(struct base_station_robot_arm_command);
 /*
     case (char) robot_arm_constant_speed_id:
         return sizeof(struct robot_arm_command);
 */
-    case (char) gps_telem_reply:
+    case gps_telem_reply:
             return sizeof(struct gps_telem);
 
     } //endswitch:		(structId)

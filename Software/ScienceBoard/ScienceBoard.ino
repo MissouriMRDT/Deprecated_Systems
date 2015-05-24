67f2d4c7569a3df52b7bec400e11f932d49d8e17
Owen Chiaventone <omc8db@mst.edu>
2015-05-23 20:55:05 -0500

    Sketch out architecture

--- Software/ScienceBoard/ScienceBoard.ino
+++ Software/ScienceBoard/ScienceBoard.ino
@@ -12,21 +12,56 @@ const int CCD_TX = 5;
 
 const int MOIS_SENSOR = 0; //Analog
 
+//--------------------
+//
+//---------------------
+const int LASER_WARMUP_TIME = 3000;
+
 void setup() {
   // put your setup code here, to run once:
   pinMode(LASER_CTR, OUTPUT);  
   Serial.begin(115200);
   
-    digitalWrite(LASER_CTR, LOW);
-  Serial.println("Laser off");
-  delay(5000);
+  //Open software serial on PH sensor
+  
+  //Open software serial on CCD at 115200 baud
+  
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 
-  digitalWrite(LASER_CTR, HIGH);
-  Serial.println("Laser on");
-  delay(5000);
+  //if(recieve science_telem_request)
+  //{
+      switch(science_telem_request.requestType)
+      {
+        case PH_type:
+          Read PH data (Software Serial);
+          Send PH data;
+          break
+        case moisture_type:
+          Read Moisture Data (analog input);
+          Send moisture data;
+          break;
+        case CCD_type:
+          digitalWrite(LASER_CTR);
+          delay(LASER_WARMUP_TIME);
+          send CCD read request;
+          delay(LASER_ON_TIME);
+          digitalWrite(LASER_CTR, LOW);
+          for(int i = 0; i < CCD_ELEMENTS; i++)
+          {
+            request element from due
+            write into struct
+            if(struct full)
+               send struct 
+          }
+          break;
+        default:
+          Error
+      }
+          
+          
+    
   
 }
---
+++ Software/ScienceBoard/structs.h
@@ -0,0 +1,38 @@
+
+//-------------------------
+//Comes in from motherboard
+//-------------------------
+
+struct science_telem_request
+{
+  byte struct_id;
+  byte requestType;
+}
+
+//request types
+const int PH_type = 1;
+const int moisture_type = 2;
+const int CCD_type = 3;
+
+//-----------------------
+//Goes out to motherboard
+//-----------------------
+
+struct PH_telem
+{
+  byte struct_id;
+  float PH;
+}
+
+struct moisture_telem
+{
+  byte struct_id;
+  short moisture;
+}
+
+struct CCD_telem_packet
+{
+  byte struct_id;
+  short packetIndex; 
+  short data[12];
+}

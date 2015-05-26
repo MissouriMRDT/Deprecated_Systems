cbffa6d3eac12bf7bf315f434710c513f3105981
Owen Chiaventone <omc8db@mst.edu>
2015-05-25 23:44:59 -0600

    Update PTZ cam for Horizon
    Embed EasyTransfer, rename to pan and tilt, and update to Horizon Rovecomm protocol

--- cam_ctrl/cam_ctrl.ino
+++ cam_ctrl/cam_ctrl.ino
@@ -9,24 +9,19 @@
   
 */
 #include <Servo.h>
-#include <EasyTransfer.h>
+#include "EasyTransfer.h"
 
 struct control_struct
 {
-  uint8_t pitch;
-  uint8_t yaw;
-  uint8_t roll;
-  uint8_t mode;
+  uint8_t struct_id;
+  uint8_t pan;
+  uint8_t tilt;
 };
 
-const int PITCH = 6; //Blue
-const int YAW = 9;   //Green
-const int ROLL = 10; //Red
-const int MODE = 11; //White
-Servo pitch;
-Servo yaw;
-//Servo roll;
-//Servo mode;
+const int PAN = 6; //Blue
+const int TILT = 9;   //Green
+Servo pan;
+Servo tilt;
 
 
 control_struct control_data;
@@ -35,32 +30,24 @@ EasyTransfer controls;
 void setup()
 {
   Serial.begin(115200);  
-  pitch.attach(PITCH);
-  yaw.attach(YAW);
-  //roll.attach(ROLL);
-  //mode.attach(MODE);
-  pinMode(PITCH, OUTPUT);
-  pinMode(YAW, OUTPUT);
-  //pinMode(ROLL, OUTPUT);
-  //pinMode(MODE, OUTPUT);
-  control_data.pitch = 90;
-  control_data.yaw = 90;
-  //control_data.roll = 90;
-  //control_data.mode = 90;
+  pan.attach(PAN);
+  tilt.attach(TILT);
+  
+  pinMode(PAN, OUTPUT);
+  pinMode(TILT, OUTPUT);
+
+  control_data.pan = 90;
+  control_data.tilt = 90;
+
   controls.begin(details(control_data), &Serial);
   
-  //Wait for camera module to initialize
-  //delay(10000);
 }
 
 void loop()
 {
 
   controls.receiveData();
-  pitch.write(control_data.pitch);
-  yaw.write(control_data.yaw);
-  //roll.write(control_data.roll);
-  //mode.write(control_data.mode);
-
+  pan.write(control_data.pan);
+  tilt.write(control_data.tilt);
   
 }

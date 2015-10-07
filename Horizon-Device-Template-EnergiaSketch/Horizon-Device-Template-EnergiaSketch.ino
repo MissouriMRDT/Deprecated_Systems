37d56d66c6f23b8805f4126b34c26a4ec58f4ac5
Judah Schad <jrs6w7@mst.edu>
2015-10-07 15:50:28 -0500

    Init Port to Energia

---
+++ Horizon-Device-Template-EnergiaSketch/Horizon-Device-Template-EnergiaSketch.ino
@@ -0,0 +1,29 @@
+// Missouri Science and Technology Mars Rover Design Team 2015
+//
+// roveDeviceTemplate
+//
+// Using Energia mspgcc compiler by Peter Bigot and is based on the Wiring and Arduino framework
+//
+// first created:
+//
+// 10_05_2015       V1 - URC Horizon IP TCP Client / UART/ PWM Controller Template
+//
+// Judah Schad jrs6w7@mst.edu
+//
+// mrdt::roveWare
+
+void setup()
+{
+  //This is analagous to the file BiosInit.main in Horizon-Device-Template-CCS-Software on the TIVA platform
+  
+  //https://github.com/MST-MRDT/Horizon-Device-Template-CCS-Software/blob/master/roveBIOS_Init.c
+  
+}//end setup
+
+void loop()
+{
+  // This is analagous to the file roveDeviceTemplate_Thread.c in Horizon-Device-Template-CCS-Software on the TIVA platform
+  
+  //https://github.com/MST-MRDT/Horizon-Device-Template-CCS-Software/blob/master/roveDeviceTemplate_Thread.c
+  
+}//end loop

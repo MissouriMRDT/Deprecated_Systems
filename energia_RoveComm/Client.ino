3af76c4809e76bfa3afa84daefa64eb98fc6d961
Gbenga <oofhc@mst.edu>
2015-10-15 15:03:36 -0500

    Setup Server portion for continous listening on port 3500 with serial print for testing

--- energia_RoveComm/Client.ino
+++ energia_RoveComm/Client.ino
@@ -1,5 +1,8 @@
 #include <SPI.h>
 #include <Ethernet.h>
+#include <EthernetUdp.h>
+
+EthernetUDP udpClient;
 
 void setupClient() {
   
--- energia_RoveComm/energia_RoveComm.ino
+++ energia_RoveComm/energia_RoveComm.ino
@@ -6,15 +6,19 @@ This project multitasks a client and a server for our chip
 
 #include <SPI.h>
 #include <Ethernet.h>
+#include <EthernetUdp.h>
+
+#define ROVECOMM_PORT 3500
 
 //device MAC Adress
 byte mac[] = {0xae, 0x00, 0x00, 0x00, 0x00, 0x00};
 
 //device assigned IP address
-byte ip[] = {192,168,0,1};
+IPAddress ip(192,168,0,1);
 
 void setup() {
   Ethernet.begin(mac, ip);
+  Serial.begin(9600);
 }
 
 void loop() {

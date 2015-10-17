7a11d193935db092fc9bf1771177e22ad8f0c5b9
Gbenga <oofhc@mst.edu>
2015-10-17 13:38:49 -0500

    Should be bundled with last commit, forgot to save

--- energia_RoveComm/energia_RoveComm.ino
+++ energia_RoveComm/energia_RoveComm.ino
@@ -1,6 +1,6 @@
 /*
 
-This project multitasks a client and a server for our chip
+This file has some client and server functions for our devices
 
 */
 
@@ -13,19 +13,56 @@ This project multitasks a client and a server for our chip
 //device MAC Adress
 byte mac[] = {0x00, 0x1a, 0xb6, 0x02, 0xe7, 0x50};
 
-//device assigned IP address
-//IPAddress ip(192,168,0,2);
+EthernetUDP udpServer;
+char serverBuffer[UDP_TX_PACKET_MAX_SIZE];
+
+void sendMsg(IPAddress ip, int port, char* msg){
+  Serial.print("Sending Msg: ");
+  Serial.println(msg);
+  EthernetUDP udpClient;
+  while (!udpClient.begin(random(30000,35000)));
+  udpClient.beginPacket(ip,port);
+    udpClient.write(msg);
+  udpClient.endPacket();
+  udpClient.stop();
+  Serial.println("Msg Sent");
+}
+
+void getAllMsg() {
+  Serial.println("Checking messages");
+  int packetSize = udpServer.parsePacket();
+  while (packetSize){
+    IPAddress remote = udpServer.remoteIP();
+    int remote_port = udpServer.remotePort();
+    Serial.print("Message from ");
+    for(int i = 0; i<4; i++) {
+      Serial.print(remote[i]);
+      if (i < 3)
+        Serial.print(".");
+    }
+    Serial.print(":");
+    Serial.println(remote_port);
+    udpServer.read(serverBuffer, UDP_TX_PACKET_MAX_SIZE);
+    Serial.println(serverBuffer);
+    Serial.println();
+    packetSize = udpServer.parsePacket();
+  } 
+}
 
 void setup() {
   Serial.begin(9600);
   if (!Ethernet.begin(mac)) {
     for (;;) {
-      Serial.print("Connection Failed");
+      Serial.println("Connection Failed");
       delay(1000);
     }
   }
+  udpServer.begin(ROVECOMM_PORT);
 }
 
 void loop() {
-  
+  char saying[] = "Hello";
+  sendMsg(IPAddress(192,168,1,102), ROVECOMM_PORT, saying);
+  getAllMsg();
+  delay(5000);
 }

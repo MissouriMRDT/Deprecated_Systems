5ae4cd9cb191a3f7c9c690b43e29c71f96ba6763
Gbenga <oofhc@mst.edu>
2015-11-03 10:23:28 -0600

    fixed Endianness of packet. Found bug: data is limited to 4 bytes, others are cut off

--- C/roveComm_Client.c
+++ C/roveComm_Client.c
@@ -48,14 +48,20 @@ int main(int argc, char* argv[])
   }
   
   
-  char buffer[1024];
+  uint8_t buffer[1024];
  
-  memcpy(&buffer[0], &verNum, 1);
-  memcpy(&buffer[1], &seqNum, 2);
-  memcpy(&buffer[3], &dataID, 2);
-  memcpy(&buffer[5], &dataSize, 2);
-  memcpy(&buffer[7], hexData, dataSize);
+  buffer[0] = verNum;
+  buffer[1] = seqNum >> 8;
+  buffer[2] = seqNum;
+  buffer[3] = dataID >> 8;
+  buffer[4] = dataID;
+  buffer[5] = dataSize >> 8;
+  buffer[6] = dataSize;
   
+  int i;
+  for (i=0; i<dataSize; i++)
+    buffer[7+i] = hexData[dataSize -1 -i];
+    
   /* create an Internet, datagram, socket using UDP */
   sock = socket(PF_INET, SOCK_DGRAM, IPPROTO_UDP);
   if (-1 == sock) {
@@ -73,15 +79,19 @@ int main(int argc, char* argv[])
   /* sockets are unsigned shorts, htons(x) ensures x is in network byte order, set the port to our given or default port */
   sa.sin_port = htons(destinationPort);
  
-  bytes_sent = sendto(sock, buffer, strlen(buffer) + 1, 0,(struct sockaddr*)&sa, sizeof sa);
-  printf ("Sent %d bytes: %s\n", bytes_sent, buffer);
+  bytes_sent = sendto(sock, buffer, dataSize + 7, 0,(struct sockaddr*)&sa, sizeof sa);
+  printf ("Sent %d bytes: ", bytes_sent);
+  for (i=0; i<dataSize + 7; i++)
+    printf("%X ", buffer[i]);
+  printf("\n");
+  
   if (bytes_sent < 0) {
     printf("Error sending packet: %s\n", strerror(errno));
     exit(EXIT_FAILURE);
   }
 
-  bytes_sent = recvfrom(sock, (void *)buffer, sizeof(buffer), 0, (struct sockaddr*)&sa, &fromlen);
-  printf("%s\n", buffer);
+  //bytes_sent = recvfrom(sock, (void *)buffer, sizeof(buffer), 0, (struct sockaddr*)&sa, &fromlen);
+  //printf("%s\n", buffer);
  
   close(sock); /* close the socket */
   return 0;

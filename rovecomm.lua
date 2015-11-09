c865b84867234a01130bb9d25d8eb69176fa06a1
Josh Reed <jlrdkc@mst.edu>
2015-11-08 22:16:59 -0600

    Added Custom Info Column

--- rovecomm.lua
+++ rovecomm.lua
@@ -9,6 +9,9 @@ roveProtocol.fields = { fieldVersion, fieldSequenceNum, fieldDataId, fieldDataSi
 
 function roveProtocol.dissector(buffer,pinfo,tree)
         pinfo.cols.protocol = "ROVE"
+		
+		pinfo.cols['info'] = 'DataId: ' .. buffer:range(3,2):uint()
+		
         local subtree = tree:add(roveProtocol,buffer(),"MRDT RoveComm Protocol")
         subtree:add(fieldVersion,buffer(0,1))
         subtree:add(fieldSequenceNum,buffer(1,2))

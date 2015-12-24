682accf0f77b705958259ad2037bb542e2d2a4c5
Gbenga <oofhc@mst.edu>
2015-12-24 12:27:10 -0600

    converted server listen to block for only 3 seconds

--- C_Test_Files/roveComm_Server.c
+++ C_Test_Files/roveComm_Server.c
@@ -15,10 +15,12 @@ int main(void)
 {
   // Create a UDP Socket
   int sock = socket(PF_INET, SOCK_DGRAM, IPPROTO_UDP);
-  struct sockaddr_in sa; 
+  struct sockaddr_in sa, incoming; 
   char buffer[1024];
   ssize_t recsize, sendsize;
   socklen_t fromlen;
+  fd_set socketSet;
+  struct timeval timeout;
 
   memset(&sa, 0, sizeof sa);
   sa.sin_family = AF_INET;
@@ -31,25 +33,35 @@ int main(void)
     close(sock);
     exit(EXIT_FAILURE);
   }
+  
 
   for (;;) {
+    FD_ZERO(&socketSet);
+    FD_SET(sock,&socketSet);
+    timeout.tv_usec = 0;
+    timeout.tv_sec = 3;
+    select(sock +1, &socketSet,NULL,NULL,&timeout);
+    if (FD_ISSET(sock, &socketSet)){
+      memcpy(&incoming, &sa, fromlen);
+      
+      recsize = recvfrom(sock, (void*)buffer, sizeof buffer, 0, (struct sockaddr*)&incoming, &fromlen);
 
-    recsize = recvfrom(sock, (void*)buffer, sizeof buffer, 0, (struct sockaddr*)&sa, &fromlen);
-
-    if (recsize < 0) {
-      fprintf(stderr, "%s\n", strerror(errno));
-      exit(EXIT_FAILURE);
-    }
-    
-    int i = 0;
-    for (i=0; i<recsize; i++){
-      printf("%X ", (uint8_t)buffer[i]);
-    }
-    printf("\n");
-    printf("recsize: %d\n", (int)recsize);
-    sa.sin_port = htons(11000);
-    sendsize = sendto(sock, buffer, recsize, 0, (struct sockaddr*)&sa, sizeof(sa));
-    printf("Sent %d bytes\n", (int)sendsize);
-    sleep(1);
+      if (recsize < 0) {
+        fprintf(stderr, "%s\n", strerror(errno));
+        exit(EXIT_FAILURE);
+      }
+      
+      int i = 0;
+      for (i=0; i<recsize; i++){
+        printf("%X ", (uint8_t)buffer[i]);
+      }
+      printf("\n");
+      printf("recsize: %d\n", (int)recsize);
+      sa.sin_port = htons(11000);
+      sendsize = sendto(sock, buffer, recsize, 0, (struct sockaddr*)&incoming, sizeof(incoming));
+      printf("Sent %d bytes\n", (int)sendsize);
+      sleep(1);
+    } else
+      printf("%s", "No packet\n");
   }
 }

09bed7eb505ae9b30091fefa9762b07ac61e5e19
Judah Schad <jrs6w7@mst.edu>
2016-01-12 14:25:36 -0600

    Begin Capture And Compare Config Model

--- RoveBoard/RoveBoardMonitor.h
+++
@@ -1,9 +0,0 @@
-// Missouri Science and Technology Mars Rover Design Team 2015_2016
-// jrs6w7@mst.edu
-//
-// mrdt::roveWare
-#pragma once
-#ifndef ROVEBOARDMONITOR_H_
-#define ROVEBOARDMONITOR_H_
-
-#endif // ROVEMONITOR_H_

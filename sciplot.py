ac88890ffcce9300ce4761c50f96a9c4f3a95b15
Matthew Healy <mhrh3@r03mhrh3.device.mst.edu>
2016-02-02 16:30:04 -0600

    first commit

--- sciplot.py
+++ sciplot.py
@@ -1,30 +1,35 @@
-######## Missouri University of Science and Technology########
-#        Mars Rover Design Team                              #
-#        Author: Matthew Healy    mhrh3@mst.edu              #
-#                                                            #
-#    SciPlot is a graphing module that plots data from a     #
-#    serial input in real time.                              #
-#                                                            #
-#  Usage: python3.4 sciplot.py [-h] [-p] [-b] [-f] [-m] [-s] #
-##############################################################
+##############Missouri University of Science and Technology#############
+#                        Mars Rover Design Team                        #
+#        Author: Matthew Healy    mhrh3@mst.edu                        #
+#                                                                      #
+#        serial input in real time.                                    #
+#                                                                      #
+#        Usage: python3.4 sciplot.py [-h] [-p] [-b] [-f] [-m] [-s]     #
+#                                                                      #
+#   Note: This script should be run from within the directory          #
+#           That you wish to use to store the .csv file on exit, as    #
+#           saves to the current directory                             #
+########################################################################
 
 
 
-import serial 						
-import numpy						
-import matplotlib.pyplot as plt
-import csv
-import argparse
+import serial                   # used for .readline()					
+import numpy			# used for arange()			
+import matplotlib.pyplot as plt # used for all of plotting
+import csv                      # used for csv.writer() and writerow()
+import argparse                 # used for argparse.ArgumentParser()
+
+# used for auto-selection of serial ports
 import glob
 import sys
 
-global count
+global count                    # data count
 global MAX_PLOT_SIZE
-global beginData
-global endData
+global beginData                # beginning of x range
+global endData                  # ending of x range
 global filename
 global port
-global sensor
+global sensorType
 
 count = 0
 relativeCount = []
@@ -32,13 +37,11 @@ plt.ion()
 dataStore = []
 graphStore = []
 
-MAX_PLOT_SIZE = 7
 
 dataRange = []
 beginData = 0
 endData = 0
 
-plt.plot(graphStore, 'r,', label='Humidity')
 port = 0
 baudrate = 9600
 
@@ -50,7 +53,10 @@ def serial_ports():
             On unsupported or unknown platforms
         :returns:
             A list of the serial ports available on the system
+        
     """
+    # parses name of operating system and assigns corrolating prefix for serial port
+    
     if sys.platform.startswith('win'):
         ports = ['COM%s' % (i + 1) for i in range(256)]
     elif sys.platform.startswith('linux') or sys.platform.startswith('cygwin'):
@@ -72,7 +78,7 @@ def serial_ports():
     return result
 
 
-def serial_check(list_of_ports):
+def serial_check(list_of_ports): # TODO: integrate into serial_ports
     valid_ports = []
     for port in list_of_ports:
         testinput = serial.Serial(port, 9600, timeout=.001)
@@ -82,9 +88,10 @@ def serial_check(list_of_ports):
             print(float(testdata))
         except (NameError, ValueError):
             print("Serial input invalid: ",port)
+            
             pass
         else:
-            print("made it to else clause")
+            print("Valid port: ", port)
             valid_ports.append(port)
     if (len(valid_ports) == 1):
         return port
@@ -95,23 +102,29 @@ def serial_check(list_of_ports):
 
 def graph():
     global count
-    if (count < MAX_PLOT_SIZE):
-        plt.clf()
+    if sensorType=="humidity":
+        upperY = 900
+        lowerY = -5
+    elif sensorType=="temp":
+        upperY =  100
+        lowerY = -15
+    if (count < MAX_PLOT_SIZE): # holding still
+        plt.clf()               # clear current plot
         plt.scatter(count,graphStore[count])
-        plt.ylim(-5,100)
+        plt.ylim(lowerY,upperY)
         plt.grid(True)
-        plt.title(sensor)
+        plt.title(sensorType)
         plt.plot(graphStore)
         plt.show()
         count += 1
-    else:
-        plt.clf()
+    else:                       # start scrolling
+        plt.clf()               # clear current plot
         endData = count+1
         beginData = (count - MAX_PLOT_SIZE)
         dataRange = numpy.arange(beginData,(endData))
-        plt.ylim(-5,100)
+        plt.ylim(lowerY,upperY)
         plt.grid(True)
-        plt.title(sensor)
+        plt.title(sensorType)
         plt.scatter(dataRange,graphStore)
         plt.plot(dataRange,graphStore)
         plt.show
@@ -125,22 +138,22 @@ def main():
         serialport = serial_check(portslist)
         
         parser = argparse.ArgumentParser(description='plot scientific data in real time')
-        parser.add_argument('-p', default=serialport, action="store", dest="port",help="name of serial input")
+        parser.add_argument('-p', default='/dev/tty.usbmodem1411', action="store", dest="port",help="name of serial input")
         parser.add_argument('-b', default=9600, action="store", dest="baudrate", help="baudrate for data transfer")
         parser.add_argument('-f', default="sensor_data", action="store", dest="filename", help="name of file for data to be stored in")
         parser.add_argument('-m', default=7, action="store", dest="MAX_PLOT_SIZE", help="maximum size for x axis (recommended < 10)")
-        parser.add_argument('-s', default="Sensor Data", action="store", dest="sensor", help="sensor being used, in order to correctly title plot")
+        parser.add_argument('-s', default="humidity", action="store", dest="sensor", help="sensor being used, in order to correctly title plot")
 
         args = parser.parse_args()
         global MAX_PLOT_SIZE
         global filename
         global port
-        global sensor
+        global sensorType
         MAX_PLOT_SIZE = args.MAX_PLOT_SIZE
         port = args.port
         filename = args.filename + '.csv'
         serialData = serial.Serial(port, baudrate)
-        sensor = args.sensor
+        sensorType = args.sensor
         while(True):
                 
                 try:

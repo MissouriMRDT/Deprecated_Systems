67e4e93f98a6baebccd4af0f5bf9f568dc493169
Ian Broeder <Ian Broeder>
2016-02-26 22:01:22 -0600

    Ported keenans phoenix code UNTESTED

--- GPSdev/echo/echo/echo.ino
+++ GPSdev/echo/echo/echo.ino
@@ -1,26 +1,12 @@
 // Test code for Adafruit GPS modules using MTK3329/MTK3339 driver
-//
-// This code just echos whatever is coming from the GPS unit to the
-// serial monitor, handy for debugging!
-//
-// Tested and works great with the Adafruit Ultimate GPS module
 // using MTK33x9 chipset
 //    ------> http://www.adafruit.com/products/746
-// Pick one up today at the Adafruit electronics shop 
-// and help support open source hardware & software! -ada
 
+#include <SoftwareSerial.h>
 #include <Adafruit_GPS.h>
-#if ARDUINO >= 100
- #include <SoftwareSerial.h>
-#else
-  // Older Arduino IDE requires NewSoftSerial, download from:
-  // http://arduiniana.org/libraries/newsoftserial/
-// #include <NewSoftSerial.h>
- // DO NOT install NewSoftSerial if using Arduino 1.0 or later!
-#endif
 
 // Connect the GPS Power pin to 5V
-// Connect the GPS Ground pin to ground
+// Connect the GPS Ground pin to ground'''';;';
 // If using software serial (sketch example default):
 //   Connect the GPS TX (transmit) pin to Digital 3
 //   Connect the GPS RX (receive) pin to Digital 2
@@ -28,61 +14,125 @@
 //   Connect the GPS TX (transmit) pin to Arduino RX1, RX2 or RX3
 //   Connect the GPS RX (receive) pin to matching TX1, TX2 or TX3
 
-// If using software serial, keep these lines enabled
-// (you can change the pin numbers to match your wiring):
-#if ARDUINO >= 100
-  SoftwareSerial mySerial(3, 2);
-#else
-  NewSoftSerial mySerial(3, 2);
-#endif
+SoftwareSerial mySerial(3, 2);
 Adafruit_GPS GPS(&mySerial);
+
 // If using hardware serial (e.g. Arduino Mega), comment
-// out the above six lines and enable this line instead:
+// out the above lines and enable this line instead:
 //Adafruit_GPS GPS(&Serial1);
+
+struct GPS_Data
+{
+  uint8_t fix;
+  uint16_t latitude_whole;
+  uint16_t latitude_frac;
+  uint16_t longitude_whole;
+  uint16_t longitude_frac;
+  uint16_t altitude_whole;
+  uint16_t altitude_frac;
+  uint8_t lat;
+  uint8_t lon;
+  uint8_t satellites;
+};
+
+GPS_Data gps_data;
+#define data_delay 50
+
+void GPS_setup(Adafruit_GPS &GPS, GPS_Data &gps_data)
+{
+  GPS.begin(9600);
+  GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCGGA);
+  GPS.sendCommand(PMTK_SET_NMEA_UPDATE_1HZ);
+  GPS.sendCommand(PGCMD_ANTENNA);
+  
+  //setting default values for GPS data
+  gps_data.fix = GPS.fix = 0;
+  gps_data.latitude_whole = 0;
+  gps_data.latitude_frac = 0;
+  gps_data.longitude_whole = 0;
+  gps_data.longitude_frac = 0;
+  gps_data.altitude_whole = 0;
+  gps_data.altitude_frac = 0;
+  gps_data.lat = 'X';
+  gps_data.lon = 'X';
+  gps_data.satellites = 0;
+  
+  //settings for GPS timer
+  OCR0A = 0xAF;
+  TIMSK0 |= _BV(OCIE0A);
+}
+
+SIGNAL(TIMER0_COMPA_vect)
+{
+  char c = GPS.read(); 
+}
 
 void setup()  
 {    
-  // connect at 115200 so we can read the GPS fast enuf and
-  // also spit it out
   Serial.begin(9600);
   Serial.println("Adafruit GPS library basic test!");
+  GPS_setup(GPS, gps_data);
 
-  // 9600 NMEA is the default baud rate for MTK - some use 4800
-  GPS.begin(9600);
-  
-  // You can adjust which sentences to have the module emit, below
-  
-  // uncomment this line to turn on RMC (recommended minimum) and GGA (fix data) including altitude
-  GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCGGA);
-  // uncomment this line to turn on only the "minimum recommended" data for high update rates!
-  //GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCONLY);
-  // uncomment this line to turn on all the available data - for 9600 baud you'll want 1 Hz rate
-  //GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_ALLDATA);
-  
-  // Set the update rate
-  // Note you must send both commands below to change both the output rate (how often the position
-  // is written to the serial line), and the position fix rate.
-  // 1 Hz update rate
-  //GPS.sendCommand(PMTK_SET_NMEA_UPDATE_1HZ);
-  //GPS.sendCommand(PMTK_API_SET_FIX_CTL_1HZ);
-  // 5 Hz update rate- for 9600 baud you'll have to set the output to RMC or RMCGGA only (see above)
-  GPS.sendCommand(PMTK_SET_NMEA_UPDATE_5HZ);
-  GPS.sendCommand(PMTK_API_SET_FIX_CTL_5HZ);
-  // 10 Hz update rate - for 9600 baud you'll have to set the output to RMC only (see above)
-  // Note the position can only be updated at most 5 times a second so it will lag behind serial output.
-  //GPS.sendCommand(PMTK_SET_NMEA_UPDATE_10HZ);
-  //GPS.sendCommand(PMTK_API_SET_FIX_CTL_5HZ);
-
-  // Request updates on antenna status, comment out to keep quiet
-  GPS.sendCommand(PGCMD_ANTENNA);
- 
   delay(1000);
+  
 }//end setup
 
-void loop()                   
+uint32_t timer = millis();
+
+void loop()
 {
-   char c = GPS.read();
-   Serial.println(c);
-   Serial.println("c");  // do nothing! all reading and printing is done in the interrupt
-}//end loop
+  if(GPS.newNMEAreceived())
+  {
+    if(!GPS.parse(GPS.lastNMEA()))
+      return; //if fails to parse, go back to top of loop
+  }
+  
+  //if millis or timer overflows, reset
+  if(timer > millis())
+    timer = millis();
+  
+  if(millis() - timer > data_delay)
+  {
+    timer = millis();
+    
+    gps_data.fix = GPS.fix;
+    
+    if(GPS.fix)
+    {
+      gps_data.latitude_whole = GPS.latitude;
+      gps_data.latitude_frac = (GPS.latitude-gps_data.latitude_whole)*1000;
+      gps_data.lat = GPS.lat;
+      gps_data.longitude_whole = GPS.longitude;
+      gps_data.longitude_frac = (GPS.longitude-gps_data.longitude_whole)*1000;
+      gps_data.lon = GPS.lon;
+      gps_data.altitude_whole = GPS.altitude;
+      gps_data.altitude_frac = (GPS.altitude-gps_data.altitude_whole)*1000; 
+      gps_data.satellites = GPS.satellites;
+    }
+   
+  Serial.print("gps_data.fix: "); 
+  Serial.println(gps_data.fix);
+  Serial.print("gps_data.latitude_whole: ");
+  Serial.println(gps_data.latitude_whole);
+  Serial.print("gps_data.latitude_frac: ");
+  Serial.println(gps_data.latitude_frac);
+  Serial.print("gps_data.longitude_whole: ");
+  Serial.println(gps_data.longitude_whole);
+  Serial.print("gps_data.longitude_frac: ");
+  Serial.println(gps_data.longitude_frac);
+  Serial.print("gps_data.altitude_whole: ");
+  Serial.println(gps_data.altitude_whole);
+  Serial.print("gps_data.altitude_frac: ");
+  Serial.println(gps_data.altitude_frac);
+  Serial.print("gps_data.lat: ");
+  Serial.println(gps_data.lat);
+  Serial.print("gps_data.lon: ");
+  Serial.println(gps_data.lon);
+  Serial.print("gps_data.satellites: ");
+  Serial.println(gps_data.satellites);
+} //end loop
+
+
+
+
 

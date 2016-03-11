84e1b2d5d9608dd31fe7813b91e4afe3b2d810a3
John Maruska <jwmbq6@mst.edu>
2016-03-10 19:00:20 -0600

    Changed whatever

---
+++ Science-Software.ino
@@ -0,0 +1,18 @@
+#include <SPI.h>
+#include <Ethernet.h>
+#include <EthernetUdp.h>
+
+#include "RoveEthernet.h"
+#include "RoveComm.h"
+
+void setup()
+{
+  // put your setup code here, to run once:
+  
+}
+
+void loop()
+{
+  // put your main code here, to run repeatedly:
+  
+}

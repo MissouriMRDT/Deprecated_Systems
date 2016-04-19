5985722742691f27bf9d7c6d2aabd2eda9e1dfaa
John Maruska <jwmbq6@mst.edu>
2016-04-19 17:58:16 -0500

    Added confirmed sensor functions
    All sensor functions added have been tested with the hardware we
    currently have available.

--- Science-Software.ino
+++ Science-Software.ino
@@ -1,18 +1,41 @@
-#include <SPI.h>
-#include <Ethernet.h>
-#include <EthernetUdp.h>
+// Sensor libraries
+#include "Energia.h"
+#include <SHT10.h>
+#include <StellarisDS18B20.h>
 
-#include "RoveEthernet.h"
-#include "RoveComm.h"
+void setup(){}
+void loop(){}
 
-void setup()
+float RoveSci_DS18B20_ReadTemp(int data_pin)
 {
-  // put your setup code here, to run once:
-  
+  int DS18B20_Pin = data_pin;
+  DS18B20 ds(DS18B20_Pin); // split into DS18B20_Begin? removes param
+  return float(ds.GetData());
 }
 
-void loop()
+int RoveSci_Grove_ReadHumid(int data_pin)
 {
-  // put your main code here, to run repeatedly:
-  
+  int sensorValue = analogRead(data_pin) * 100 / 4095;
+  return  sensorValue;
+}
+
+int RoveSci_FC28_ReadHumid(int data_pin)
+{
+  int percentage = -(analogRead(data_pin) - 4095) * 100 / 1850;
+  if (percentage >= 100)
+    Serial.println(100);
+  else
+    Serial.println(percentage); 
+}
+
+float RoveSci_SHT10_ReadTemp(int data_pin, int clock_pin)
+{
+  SHT10 sht(data_pin, clock_pin); // same deal as DS18B20
+  return sht.readTemperature(); 
+}
+
+float RoveSci_SHT10_ReadHumid(int data_pin, int clock_pin)
+{
+  SHT10 sht(data_pin, clock_pin);
+  return sht.readHumidity(); 
 }

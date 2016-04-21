e195a11d9ffa129d17021c0c7eae036a8d73037f
John Maruska <jwmbq6@mst.edu>
2016-04-21 00:49:16 -0500

    Restructuring and roveComm integration
    Integrated roveComm and shifted some code into a header file. Currently
    unverified (fix sht10 library issue). Need to figure library includes
    for RoveSci like was done in RoveComm.

--- RoveSci.cpp
+++ RoveSci.cpp
@@ -1,39 +1,32 @@
 #include <SHT10.h>
 #include <StellarisDS18B20.h>
-#include "Energia.h"
+#include "Arduino.h"
 
-float RoveSci_DS18B20_ReadTemp(int data_pin);
-int RoveSci_Grove_ReadHumid(int data_pin);
-int RoveSci_FC28_ReadHumid(int data_pin);
-float RoveSci_SHT10_ReadHumid(int data_pin, int clock_pin);
-float RoveSci_SHT10_ReadTemp(int data_pin, int clock_pin);
-
-
-float RoveSci_DS18B20_ReadTemp(int data_pin) {
+float roveSci_DS18B20_ReadTemp(int data_pin) {
   int DS18B20_Pin = data_pin;
   DS18B20 ds(DS18B20_Pin); // split into DS18B20_Begin? removes param
   return float(ds.GetData());
 }
 
-int RoveSci_Grove_ReadHumid(int data_pin) {
-  int sensorValue = analogRead(data_pin) * 100 / 4095;
+int16_t roveSci_Grove_ReadHumid(int data_pin) {
+  int16_t sensorValue = analogRead(data_pin) * 100 / 4095;
   return  sensorValue;
 }
 
-int RoveSci_FC28_ReadHumid(int data_pin) {
-  int percentage = -(analogRead(data_pin) - 4095) * 100 / 1850;
+int16_t roveSci_FC28_ReadHumid(int data_pin) {
+  int16_t percentage = -(analogRead(data_pin) - 4095) * 100 / 1850;
   if (percentage >= 100)
-    Serial.println(100);
+	return 100;
   else
-    Serial.println(percentage); 
+    return percentage
 }
 
-float RoveSci_SHT10_ReadTemp(int data_pin, int clock_pin) {
+float roveSci_SHT10_ReadTemp(int data_pin, int clock_pin) {
   SHT10 sht(data_pin, clock_pin); // same deal as DS18B20
   return sht.readTemperature(); 
 }
 
-float RoveSci_SHT10_ReadHumid(int data_pin, int clock_pin) {
+int16_t roveSci_SHT10_ReadHumid(int data_pin, int clock_pin) {
   SHT10 sht(data_pin, clock_pin);
-  return sht.readHumidity(); 
+  return static_cast<int16_t>(sht.readHumidity()); 
 }
--- Science-Software.ino
+++ Science-Software.ino
@@ -1,11 +1,98 @@
 #include "RoveWare.h"
 #include "RoveSci.h"
 
-void setup(){
+const int PIN_T1 = 1;
+const int PIN_T2 = 2;
+const int PIN_M1 = 3;
+const int PIN_M2 = 4;
+const int PIN_M3 = 5;
 
+const byte GROVE_ENABLE = 9;
+const byte GROVE_DISABLE = 10;
+const byte SHT10_H_ENABLE = 11;
+const byte SHT10_H_DISABLE = 12; 
+const byte FC28_ENABLE = 13;
+const byte FC28_DISABLE = 14;
+
+const byte DS18B20_ENABLE = 1;
+const byte DS18B20_DISABLE = 2;
+const byte SHT10_T_ENABLE = 3; 
+const byte SHT10_T_DISABLE = 4;
+
+void setup(){
+  roveComm_Begin(192, 168, 1, 135); // predetermined science board IP
+  bool grove_on = false;
+  bool sht10_h_on = false;
+  bool fc28_on = false;
+  bool ds18b20_on = false;
+  bool sht10_t_on = false;
 }
 
 void loop(){
-
+   float dataRead;
+   int dataID = 0;
+   size_t size = 0;
+   byte receivedMsg[1];
+   
+   // check 
+   roveComm_GetMsg(&dataID, &size, receivedMsg);
+   
+   // nest within a loop to continually run between enable and disable
+   if(dataID == 1808)
+   {
+      switch(receivedMsg[0]){
+        case GROVE_ENABLE:
+          grove_on = true;
+          break; 
+        case GROVE_DISABLE:
+          grove_on = false;
+          break;
+        case SHT10_H_ENABLE:
+          sht10_h_on = true;
+          break;
+        case SHT10_H_DISABLE:
+          sht10_h_on = false;
+          break;
+        case FC28_ENABLE:
+          fc28_on = true;
+          break;
+        case FC28_DISABLE:
+          fc28_on = false;
+          break;
+        case DS18B20_ENABLE:
+          ds18b20_on = true;
+          break;
+        case DS18B20_DISABLE:
+          ds18b20_on = true;
+          break;
+        case SHT10_T_ENABLE:
+          sht10_t_on = true;
+          break;
+        case SHT10_T_DISABLE:
+          sht10_t_on = false;
+          break;
+      }
+   }
+   
+   if(grove_on) {   
+     dataRead = roveSci_Grove_ReadHumid(PIN_M1);
+     roveComm_SendMsg(0x728, sizeof(dataRead), dataRead);
+   } 
+   if(sht10_h_on) {
+     dataRead = roveSci_SHT10_ReadHumid(PIN_M2);
+     roveComm_SendMsg(0x729, sizeof(dataRead), dataRead);
+   }
+   if(fc28_on) {
+     dataRead = roveSci_FC28_ReadHumid(PIN_M3);
+     roveComm_SendMsg(0x72A, sizeof(dataRead), dataRead);
+   }
+   if(ds18b20_on) {
+     dataRead = roveSci_DS18B20_ReadTemp(PIN_T1);
+     roveComm_SendMsg(0x720, sizeof(dataRead), dataRead);
+   }
+   if(sht10_t_on) {
+     dataRead = roveSci_SHT10_ReadTemp(PIN_T2);
+     roveComm_SendMsg(0x721, sizeof(dataRead), dataRead);
+   }
 }
 

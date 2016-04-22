ddb7294a7718b1bf9d5a3ce854d40d8d42f10a7b
John Maruska <jwmbq6@mst.edu>
2016-04-22 15:11:28 -0500

    Changed RoveSci to Arduino, comm added to sci board

--- RoveSci.h
+++ RoveSci.h
@@ -1,10 +1,10 @@
 #ifndef ROVESCI_H
 #define ROVESCI_H
 
-int16_t roveSci_Grove_ReadHumid(int data_pin);
-int16_t roveSci_FC28_ReadHumid(int data_pin);
-int16_t roveSci_SHT10_ReadHumid(int data_pin, int clock_pin);
-float roveSci_SHT10_ReadTemp(int data_pin, int clock_pin);
+float roveSci_Grove_ReadHumid(int data_pin);
+float roveSci_FC28_ReadHumid(int data_pin);
+//float roveSci_SHT10_ReadHumid(int data_pin, int clock_pin);
+//float roveSci_SHT10_ReadTemp(int data_pin, int clock_pin);
 float roveSci_DS18B20_ReadTemp(int data_pin);
 
 #endif
--- Science-Software.ino
+++ Science-Software.ino
@@ -1,31 +1,25 @@
-#include "RoveWare.h"
-#include "RoveSci.h"
-
-const int PIN_T1 = 1;
-const int PIN_T2 = 2;
-const int PIN_M1 = 3;
-const int PIN_M2 = 4;
-const int PIN_M3 = 5;
-
-const byte GROVE_ENABLE = 9;
-const byte GROVE_DISABLE = 10;
-const byte SHT10_H_ENABLE = 11;
-const byte SHT10_H_DISABLE = 12; 
-const byte FC28_ENABLE = 13;
-const byte FC28_DISABLE = 14;
-
 const byte DS18B20_ENABLE = 1;
 const byte DS18B20_DISABLE = 2;
 const byte SHT10_T_ENABLE = 3; 
 const byte SHT10_T_DISABLE = 4;
 
+const byte XD28_ENABLE = 9;
+const byte XD28_DISABLE = 10;
+const byte SHT10_H_ENABLE = 11;
+const byte SHT10_H_DISABLE = 12; 
+const byte FC28_ENABLE = 13;
+const byte FC28_DISABLE = 14;
+
+const int SCI_CMD = 1808;
+
 void setup(){
   roveComm_Begin(192, 168, 1, 135); // predetermined science board IP
-  bool grove_on = false;
+  bool xd28_on = false;
   bool sht10_h_on = false;
   bool fc28_on = false;
   bool ds18b20_on = false;
   bool sht10_t_on = false;
+  Serial6.begin(9600);
 }
 
 void loop(){
@@ -34,65 +28,98 @@ void loop(){
    size_t size = 0;
    byte receivedMsg[1];
    
-   // check 
+   // Get command from base station
    roveComm_GetMsg(&dataID, &size, receivedMsg);
    
-   // nest within a loop to continually run between enable and disable
-   if(dataID == 1808)
+   
+   if(dataID == SCI_CMD)
    {
-      switch(receivedMsg[0]){
-        case GROVE_ENABLE:
-          grove_on = true;
-          break; 
-        case GROVE_DISABLE:
-          grove_on = false;
-          break;
-        case SHT10_H_ENABLE:
-          sht10_h_on = true;
-          break;
-        case SHT10_H_DISABLE:
-          sht10_h_on = false;
-          break;
-        case FC28_ENABLE:
-          fc28_on = true;
-          break;
-        case FC28_DISABLE:
-          fc28_on = false;
-          break;
-        case DS18B20_ENABLE:
-          ds18b20_on = true;
-          break;
-        case DS18B20_DISABLE:
-          ds18b20_on = true;
-          break;
-        case SHT10_T_ENABLE:
-          sht10_t_on = true;
-          break;
-        case SHT10_T_DISABLE:
-          sht10_t_on = false;
-          break;
-      }
+     //////////////////////////  
+     // enable devices block //
+     //////////////////////////
+     switch(receivedMsg[0]){
+       case XD28_ENABLE:
+         xd28_on = true;
+         break; 
+       case XD28_DISABLE:
+         grove_on = false;
+         break;
+       case SHT10_H_ENABLE:
+         sht10_h_on = true;
+         break;
+       case SHT10_H_DISABLE:
+         sht10_h_on = false;
+         break;
+       case FC28_ENABLE:
+         fc28_on = true;
+         break;
+       case FC28_DISABLE:
+         fc28_on = false;
+         break;
+       case DS18B20_ENABLE:
+-         ds18b20_on = true;
+         break;
+       case DS18B20_DISABLE:
+         ds18b20_on = true;
+         break;
+       case SHT10_T_ENABLE:
+         sht10_t_on = true;
+         break;
+       case SHT10_T_DISABLE:
+         sht10_t_on = false;
+         break;
+       case DRILL_FWD:
+        drill_on = true;
+        break;
+       case DRILL_STOP: 
+        drill_on = false;
+        break;
+     }
    }
    
-   if(grove_on) {   
-     dataRead = roveSci_Grove_ReadHumid(PIN_M1);
+   // tell drill to send reading back
+   // create delay (while(!available)? handle infs
+   // take reading back from drill
+   // send back to base station
+   
+   
+   ///////////////////////////
+   // Sensor controls block //
+   ///////////////////////////
+   if(xd28_on) {   
+     Serial6.write(XD28_ENABLE);
+     dataRead = Serial6.read();
      roveComm_SendMsg(0x728, sizeof(dataRead), dataRead);
    } 
    if(sht10_h_on) {
-     dataRead = roveSci_SHT10_ReadHumid(PIN_M2);
+     Serial6.write(SHT10_H_ENABLE);     
+     dataRead = Serial6.read();
      roveComm_SendMsg(0x729, sizeof(dataRead), dataRead);
    }
    if(fc28_on) {
-     dataRead = roveSci_FC28_ReadHumid(PIN_M3);
+     Serial6.write(FC28_ENABLE);
+     dataRead = Serial6.read();
      roveComm_SendMsg(0x72A, sizeof(dataRead), dataRead);
    }
    if(ds18b20_on) {
-     dataRead = roveSci_DS18B20_ReadTemp(PIN_T1);
+     Serial6.write(DS18B20_ENABLE);
+     dataRead = Serial6.parseFloat();
      roveComm_SendMsg(0x720, sizeof(dataRead), dataRead);
    }
    if(sht10_t_on) {
-     dataRead = roveSci_SHT10_ReadTemp(PIN_T2);
+     Serial6.write(SHT10_T_ENABLE);
+     dataRead = Serial6.parseFloat();
      roveComm_SendMsg(0x721, sizeof(dataRead), dataRead);
    }
+   
+   //////////////////////////
+   // Drill controls block //
+   //////////////////////////
+   if(drill_on) {
+     
+   }
+   else if(!drill_on) {
+     
+   } 
 }
 

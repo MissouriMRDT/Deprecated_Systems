f1b93ebf32bff9be06b60430364684030bc30db0
John Maruska <jwmbq6@mst.edu>
2016-05-05 17:32:28 -0500

    EasyTransfer Implementation w Judah

--- ScienceBoard-Software.ino
+++ ScienceBoard-Software.ino
@@ -27,9 +27,9 @@ static const int LASER_PIN      = PF_3;
 //       RoveComm DataID        //
 //////////////////////////////////
 
-static const int SCI_CMD        = 1808;
-static const int DRILL_CMD      = 866;
-static const int CAROUSEL_CMD   = 1809;
+static const int SCI_CMD_ID      = 1808;
+static const int DRILL_CMD_ID    = 866;
+static const int CAROUSEL_CMD_ID = 1809;
 
 //////////////////////////////////
 // RoveComm received messages   //
@@ -48,7 +48,8 @@ static const byte M1_ENABLE     = 9;
 static const byte M1_DISABLE    = 10; 
 static const byte M2_ENABLE     = 11;
 static const byte M2_DISABLE    = 12;
-// M3 does not exist
+static const byte M3_ENABLE     = 13;
+static const byte M3_DISABLE    = 14;
 static const byte M4_ENABLE     = 15;
 static const byte M4_DISABLE    = 16;
 
@@ -57,6 +58,9 @@ static const byte LASER_ENABLE  = 18;
 static const byte LASER_DISABLE = 19;
 static const byte FUNNEL_OPEN   = 20;
 static const byte FUNNEL_CLOSE  = 21;
+
+static const byte CS_ENABLE     = 22;
+static const byte CS_DISABLE    = 23;
 
 static const byte DRILL_STOP    = 0;
 static const byte DRILL_FWD     = 1;
@@ -74,7 +78,8 @@ static const int T4_ON           = 6;
 
 static const int M1_ON           = 7;
 static const int M2_ON           = 8;
-static const int M4_ON           = 9;
+static const int M3_ON           = 9;
+static const int M4_ON           = 10;
 
 //////////////////////////////////
 //    Device States Variables   //
@@ -86,8 +91,9 @@ bool t3_on = false;
 bool t4_on = false;
 bool m1_on = false;
 bool m2_on = false;
-// M3 does not exist
+bool m3_on = false;
 bool m4_on = false;
+bool cs_on = false;
 
 int drill_mode = 0;
 
@@ -95,23 +101,33 @@ int drill_mode = 0;
 //    EasyTransfer Protocol     //
 //////////////////////////////////
 
-EasyTransfer ETin, ETout; // TODO : Change names after confirmed working
+EasyTransfer FromDrillBoard, ToDrillBoard;
 
 struct RECEIVE_DATA_STRUCTURE { // Must match drill side
-  float sensor_data[TOTAL_SENSORS];  // TODO : How important is latency?
-                                     // Do these reset to 0 after each send? Hold value? What if something unassigned?
-  int sensor_id[TOTAL_SENSORS];      // Should size be made to optimize send time (8) or readability (10)
+  float t1_data;
+  float t2_data;
+  float t3_data;
+  float t4_data;
+  float m1_data;
+  float m2_data;
+  float m3_data;
+  float m4_data;
+  float drill_current;
 };
 
 struct SEND_DATA_STRUCTURE { // Must match drill side
-  bool sensor_state[TOTAL_SENSORS];  
-  int drill_state;
+  uint16_t drill_cmd;
 };
 
-RECEIVE_DATA_STRUCTURE data_in;
-SEND_DATA_STRUCTURE    cmd_out;
+RECEIVE_DATA_STRUCTURE receive_telem;
+SEND_DATA_STRUCTURE    send_cmd;
 
 //////////////////////////////////
+
+float dataRead;
+uint16_t dataID = 0;
+size_t size = 0;
+byte receivedMsg[1];
 
 Dynamixel Carousel;
 Servo Funnel;
@@ -121,47 +137,22 @@ void setup(){
   DynamixelInit(&Carousel, AX, 1, 7, 1000000);
   DynamixelSetMode(Carousel, Joint);
   Funnel.attach(FUNNEL_SERVO);
-  Serial6.begin(9600);
-  ETin.begin(details(data_in), &Serial);
-  ETout.begin(details(cmd_out), &Serial);
+  Serial.begin(9600);  // TODO : Just for debugging
+  FromDrillBoard.begin(details(receive_telem), &Serial6);   // TODO :  pick Serial line. Check pins.
+  ToDrillBoard.begin(details(send_cmd), &Serial6);
 }
 
 void loop(){
-   float dataRead;
-   uint16_t dataID = 0;
-   size_t size = 0;
-   byte receivedMsg[1];
-   
    // Get command from base station
    roveComm_GetMsg(&dataID, &size, receivedMsg);
    
-   if(dataID == SCI_CMD)
+   if(dataID == SCI_CMD_ID)
    {
      //////////////////////////  
      // enable devices block //
      //////////////////////////
      
      switch(receivedMsg[0]){
-       case M1_ENABLE:
-         m1_on = true;
-         break; 
-       case M1_DISABLE:
-         m1_on = false;
-         break;
-       case M2_ENABLE:
-         m2_on = true;
-         break;
-       case M2_DISABLE:
-         m2_on = false;
-         break;
-       // M3 will not be equipped. Line filled by oscillating crystal.
-       case M4_ENABLE:
-         m4_on = true;
-         break;
-       case M4_DISABLE:
-         m4_on = false;
-         break;
-         
        case T1_ENABLE:
          t1_on = true;
          break;
@@ -186,6 +177,36 @@ void loop(){
        case T4_DISABLE:
          t4_on = false;
          break;
+       
+       case M1_ENABLE:
+         m1_on = true;
+         break; 
+       case M1_DISABLE:
+         m1_on = false;
+         break;
+       case M2_ENABLE:
+         m2_on = true;
+         break;
+       case M2_DISABLE:
+         m2_on = false;
+         break;
+       // M3 will not be equipped. Line filled by oscillating crystal.
+       case M3_ENABLE:
+         m3_on = true;
+         break;
+       case M4_ENABLE:
+         m4_on = true;
+         break;
+       case M4_DISABLE:
+         m4_on = false;
+         break;
+
+       case CS_ENABLE:
+         cs_on = true;
+         break;
+       case CS_DISABLE:
+         cs_on = false;
+         break;
          
        case LASER_ENABLE:
          digitalWrite(LASER_PIN, HIGH);
@@ -202,7 +223,7 @@ void loop(){
      }
    }
    
-   if(dataID == DRILL_CMD) {
+   if(dataID == DRILL_CMD_ID) {
      switch(receivedMsg[0]){
        case DRILL_FWD:
          drill_mode = DRILL_FWD;
@@ -216,7 +237,7 @@ void loop(){
      }
    }
      
-   if(dataID == CAROUSEL_CMD)
+   if(dataID == CAROUSEL_CMD_ID)
    {
      uint16_t position = *(uint8_t*)(receivedMsg);
      if (position == 5)
@@ -224,31 +245,26 @@ void loop(){
      DynamixelRotateJoint(Carousel, position * 102);
    }
    
-   ///////////////////////////
-   // Device controls block //
-   ///////////////////////////
+   //////////////////////////////
+   // Send data to BaseStation //
+   //////////////////////////////
    
-   // TODO : Figure out how drill board receives
-   cmd_out.sensor_state[T1_ON - 3] = t1_on;
-   cmd_out.sensor_state[T2_ON - 3] = t2_on;
-   cmd_out.sensor_state[T3_ON - 3] = t3_on;
-   cmd_out.sensor_state[T4_ON - 3] = t4_on;
-   cmd_out.sensor_state[M1_ON - 3] = m1_on;
-   cmd_out.sensor_state[M2_ON - 3] = m2_on;
-   // M3 slot occupied by the oscillating crystal. No M3 moisture sensor
-   cmd_out.sensor_state[M4_ON - 3] = m4_on;
-   
-   cmd_out.drill_state = drill_mode;
-   
-   // This block is first to use the sensor_state array in EasyTransfer for which 
-   // reads are enabled instead of ANOTHER giant set of if statements or a switch case
-   for(int i = 0; i < 5; i++) { // clear buffer
-     ETin.receiveData();
+   if(FromDrillBoard.receiveData()){
+     if(t1_on)
+       roveComm_SendMsg(0x720, sizeof(receive_telem.t1_data), &receive_telem.t1_data);
+     if(t2_on)
+       roveComm_SendMsg(0x721, sizeof(receive_telem.t2_data), &receive_telem.t2_data);
+     if(t3_on)
+       roveComm_SendMsg(0x722, sizeof(receive_telem.t3_data), &receive_telem.t3_data);
+     if(t4_on)
+       roveComm_SendMsg(0x723, sizeof(receive_telem.t4_data), &receive_telem.t4_data);
+     if(m1_on)  
+       roveComm_SendMsg(0x728, sizeof(receive_telem.m1_data), &receive_telem.m1_data);
+     if(m2_on)  
+       roveComm_SendMsg(0x729, sizeof(receive_telem.m2_data), &receive_telem.m2_data);
+     if(m3_on) 
+       roveComm_SendMsg(0x72A, sizeof(receive_telem.m3_data), &receive_telem.m3_data);
+     if(m4_on) 
+       roveComm_SendMsg(0x72B, sizeof(receive_telem.m4_data), &receive_telem.m4_data);
    }
-   for(int j = 0; j < TOTAL_SENSORS; j++) { 
-     if(cmd_out.sensor_state[j]) // if sensor is enabled
-       roveComm_SendMsg(data_in.sensor_id[j], sizeof(data_in.sensor_data[j]), &data_in.sensor_data[j]);
-   }
-   
-   ETout.sendData();
 }

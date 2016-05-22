c66c14da76d629e305a26a26341084b8f6f3dd71
John Maruska <jwmbq6@mst.edu>
2016-05-21 22:38:21 -0500

    Added final touches and changed concerns to header

--- ScienceTaskGraph.py
+++ ScienceTaskGraph.py
@@ -1,8 +1,9 @@
-"""
-Concerns:
-    Need to test graph actually works (!)
-    Check to make sure plt attributes can be changed outside subplots
-"""
+##################################################################
+#          Missouri University of Science and Technology         #
+#                    Mars Rover Design Team                      #
+#       John Maruska                        jwmbq6@mst.edu       #
+#   2016 Spring Semester - Intended for use with Zenith rover    #
+##################################################################
 
 import csv
 import dateutil.parser
@@ -29,34 +30,34 @@ humid3_time = []
 humid4_time = []
 
 csv_name = input("Enter name of CSV file: ")
-
 with open(csv_name, 'r') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=' ')
     for row in csvfile.readlines():
-        if(row[1] == "Temp1"):       
-            temp1_time.append(dateutil.parser.parse(row[0]))
-            temp1_data.append(row[2])
-        elif(row[1] == "Temp2"):
-            temp2_time.append(dateutil.parser.parse(row[0]))
-            temp2_data.append(row[2])
-        elif(row[1] == "Temp3"):
-            temp3_time.append(dateutil.parser.parse(row[0]))
-            temp3_data.append(row[2])
-        elif(row[1] == "Temp4"):
-            temp4_time.append(dateutil.parser.parse(row[0]))
-            temp4_data.append(row[2])
-        elif(row[1] == "Humid1"):
-            humid1_time.append(dateutil.parser.parse(row[0]))
-            humid1_data.append(row[2])
-        elif(row[1] == "Humid2"):
-            humid2_data.append(dateutil.parser.parse(row[0]))
-            humid2_data.append(row[2])
-        elif(row[1] == "Humid3"):
-            humid3_data.append(dateutil.parser.parse(row[0]))
-            humid3_data.append(row[2])
-        elif(row[1] == "Humid4"):
-            humid4_data.append(dateutil.parser.parse(row[0]))
-            humid4_data.append(row[2])
+        datestamp, sensor, raw_data = row.split(' ')
+        if(sensor == "Temp1"):       
+            temp1_time.append(dateutil.parser.parse(datestamp))
+            temp1_data.append(raw_data)
+        elif(sensor == "Temp2"):
+            temp2_time.append(dateutil.parser.parse(datestamp))
+            temp2_data.append(raw_data)
+        elif(sensor == "Temp3"):
+            temp3_time.append(dateutil.parser.parse(datestamp))
+            temp3_data.append(raw_data)
+        elif(sensor == "Temp4"):
+            temp4_time.append(dateutil.parser.parse(datestamp))
+            temp4_data.append(raw_data)
+        elif(sensor == "Humid1"):
+            humid1_time.append(dateutil.parser.parse(datestamp))
+            humid1_data.append(raw_data)
+        elif(sensor == "Humid2"):
+            humid2_time.append(dateutil.parser.parse(datestamp))
+            humid2_data.append(raw_data)
+        elif(sensor == "Humid3"):
+            humid3_time.append(dateutil.parser.parse(datestamp))
+            humid3_data.append(raw_data)
+        elif(sensor == "Humid4"):
+            humid4_time.append(dateutil.parser.parse(datestamp))
+            humid4_data.append(raw_data)
             
 # Temperature Figure
 plt.figure(1)
@@ -108,7 +109,6 @@ plt.suptitle("Soil Temperature Readings", fontsize=17)
 plt.subplot(2,2,1)
 plt.title("Humid1")
 plt.ylabel("Water Content (%)")
-plt.xlabel("Time Sample Taken")
 plt.grid(True)
 plt.ylim(0,100)
 plt.xticks(ha='right', rotation=15)
@@ -118,7 +118,6 @@ plt.plot_date(x=humid1_time, y=humid1_data)
 plt.subplot(2,2,2)
 plt.title("Humid2")
 plt.ylabel("Water Content (%)")
-plt.xlabel("Time Sample Taken")
 plt.grid(True)
 plt.ylim(0,100)
 plt.xticks(ha='right', rotation=15)
@@ -128,7 +127,6 @@ plt.plot_date(x=humid2_time, y=humid2_data)
 plt.subplot(2,2,3)
 plt.title("Humid3")
 plt.ylabel("Water Content (%)")
-plt.xlabel("Time Sample Taken")
 plt.grid(True)
 plt.ylim(0,100)
 plt.xticks(ha='right', rotation=15)
@@ -138,7 +136,6 @@ plt.plot_date(x=humid3_time, y=humid3_data)
 plt.subplot(2,2,4)
 plt.title("Humid4")
 plt.ylabel("Water Content (%)")
-plt.xlabel("Time Sample Taken")
 plt.grid(True)
 plt.ylim(0,100)
 plt.xticks(ha='right', rotation=15)

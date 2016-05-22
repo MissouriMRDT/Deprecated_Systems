3c38bb8c7c37ce1c9187a3a7388d1e2573669eb2
John Maruska <jwmbq6@mst.edu>
2016-05-21 21:23:56 -0500

    Formatting changes to account for datetime parsing

--- ScienceTaskGraph.py
+++ ScienceTaskGraph.py
@@ -5,6 +5,7 @@ Concerns:
 """
 
 import csv
+import dateutil.parser
 import matplotlib.pyplot as plt
 
 temp1_data = []
@@ -28,34 +29,33 @@ humid3_time = []
 humid4_time = []
 
 csv_name = input("Enter name of CSV file: ")
+
 with open(csv_name, 'rb') as csvfile:
-    for line in csvfile.readlines():
-        row = line.split(' ')
-        if(row[1] == "Temp1"):        
+    for row in csvfile.readlines():
+        if(row[1] == "Temp1"):       
+            temp1_time.append(dateutil.parser.parse(row[0]))
             temp1_data.append(row[2])
-            # Parse date-time stamp 
-            temp1_time.append(row[0])
         elif(row[1] == "Temp2"):
+            temp2_time.append(dateutil.parser.parse(row[0]))
             temp2_data.append(row[2])
-            temp2_time.append(row[0])
         elif(row[1] == "Temp3"):
+            temp3_time.append(dateutil.parser.parse(row[0]))
             temp3_data.append(row[2])
-            temp3_time.append(row[0])
         elif(row[1] == "Temp4"):
+            temp4_time.append(dateutil.parser.parse(row[0]))
             temp4_data.append(row[2])
-            temp4_time.append(row[0])
         elif(row[1] == "Humid1"):
+            humid1_time.append(dateutil.parser.parse(row[0]))
             humid1_data.append(row[2])
-            humid1_time.append(row[0])
         elif(row[1] == "Humid2"):
+            humid2_data.append(dateutil.parser.parse(row[0]))
             humid2_data.append(row[2])
-            humid2_data.append(row[0])
         elif(row[1] == "Humid3"):
+            humid3_data.append(dateutil.parser.parse(row[0]))
             humid3_data.append(row[2])
-            humid3_data.append(row[0])
         elif(row[1] == "Humid4"):
+            humid4_data.append(dateutil.parser.parse(row[0]))
             humid4_data.append(row[2])
-            humid4_data.append(row[0])
             
 # Temperature Figure
 plt.figure(1)
@@ -65,38 +65,38 @@ plt.suptitle("Soil Temperature Readings", fontsize=17)
 #T1 Subplot
 plt.subplot(2,2,1)
 plt.title("Temp1")
-plt.xlabel("Time Sample Taken")
 plt.ylabel("Temperature (Celsius)")
 plt.grid(True)
 plt.ylim(0,50)
-plt.plot(x=temp1_time, y=temp1_data, 'ro')
+plt.xticks(ha='right', rotation=15)
+plt.plot_date(x=temp1_time, y=temp1_data)
 
 #T2 Subplot
 plt.subplot(2,2,2)
 plt.title("Temp2")
-plt.xlabel("Time Sample Taken")
 plt.ylabel("Temperature (Celsius)")
 plt.grid(True)
 plt.ylim(0,50)
-plt.plot(x=temp2_time, y=temp2_data, 'ro')
+plt.xticks(ha='right', rotation=15)
+plt.plot_date(x=temp2_time, y=temp2_data)
 
 #T3 Subplot
 plt.subplot(2,2,3)
 plt.title("Temp3")
-plt.xlabel("Time Sample Taken")
 plt.ylabel("Temperature (Celsius)")
 plt.grid(True)
 plt.ylim(0,50)
-plt.plot(x=temp3_time, y=temp3_data, 'ro')
+plt.xticks(ha='right', rotation=15)
+plt.plot_date(x=temp3_time, y=temp3_data)
 
 #T4 Subplot
 plt.subplot(2,2,4)
 plt.title("Temp4")
-plt.xlabel("Time Sample Taken")
 plt.ylabel("Temperature (Celsius)")
 plt.grid(True)
 plt.ylim(0,50)
-plt.plot(x=temp4_time, y=temp4_data, 'ro')
+plt.xticks(ha='right', rotation=15)
+plt.plot_date(x=temp4_time, y=temp4_data)
 
 # Humidity Figure
 plt.figure(2)
@@ -110,31 +110,37 @@ plt.ylabel("Water Content (%)")
 plt.xlabel("Time Sample Taken")
 plt.grid(True)
 plt.ylim(0,100)
-plt.plot(x=humid1_time, y=humid1_data, 'ro')
+plt.xticks(ha='right', rotation=15)
+plt.plot_date(x=humid1_time, y=humid1_data)
 
 #M2 Subplot
-plt.subplot(2,2,1)
+plt.subplot(2,2,2)
 plt.title("Humid2")
 plt.ylabel("Water Content (%)")
 plt.xlabel("Time Sample Taken")
 plt.grid(True)
 plt.ylim(0,100)
-plt.plot(x=humid2_time, y=humid2_data, 'ro')
+plt.xticks(ha='right', rotation=15)
+plt.plot_date(x=humid2_time, y=humid2_data)
 
 #M3 Subplot
-plt.subplot(2,2,1)
+plt.subplot(2,2,3)
 plt.title("Humid3")
 plt.ylabel("Water Content (%)")
 plt.xlabel("Time Sample Taken")
 plt.grid(True)
 plt.ylim(0,100)
-plt.plot(x=humid3_time, y=humid3_data, 'ro')
+plt.xticks(ha='right', rotation=15)
+plt.plot_date(x=humid3_time, y=humid3_data)
 
 #M4 Subplot
-plt.subplot(2,2,1)
+plt.subplot(2,2,4)
 plt.title("Humid4")
 plt.ylabel("Water Content (%)")
 plt.xlabel("Time Sample Taken")
 plt.grid(True)
 plt.ylim(0,100)
-plt.plot(x=humid4_time, y=humid4_data, 'ro')
+plt.xticks(ha='right', rotation=15)
+plt.plot_date(x=humid4_time, y=humid4_data)
+
+plt.show()

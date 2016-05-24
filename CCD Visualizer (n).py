5ff374e3af505e5fdb6658d51ef5ef289a45ae05
John Maruska <jwmbq6@mst.edu>
2016-05-24 17:03:54 -0500

    Changed to cycle through multiple reads
    Previous implementation just graphed all over one another at one go.
    This implementation will graph one line (i.e. one CCD read) at a time,
    clearing to show the next.

--- CCD Visualizer (n).py
+++ CCD Visualizer (n).py
@@ -3,17 +3,37 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 plt.ion()
-plt.figure()
-plt.grid(True)
-plt.ylim(0,1023)
 
-ccd_data = []
 csv_name = input("Enter name of CSV file: ")
+file = open(csv_name)
+numline = len(file.readlines())
+file.close()
+
+ccd_data = [[] for y in range(numline)]
+data_len = []
+max_len = 0
+row_count = 0
+
 with open(csv_name, 'r') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
     for row in csvfile.readlines():
-        ccd_data = row.split(',')
-        ccd_data = [int(i) for i in ccd_data]
-        data_len = len(ccd_data)
-        plt.xlim(xmax=data_len)
-        plt.plot(np.arange(start=0, stop=data_len, step=1), ccd_data)
+        ccd_data[row_count] = row.split(',')
+        ccd_data[row_count] = [int(i) for i in ccd_data[row_count]]
+        data_len.append(len(ccd_data[row_count]))
+        if len(ccd_data[row_count]) > max_len:
+            max_len = len(ccd_data[row_count])
+        row_count = row_count + 1
+
+for row in range(numline):
+    plt.delaxes()
+    plt.grid(True)
+    plt.xlim(xmin=0, xmax=max_len)
+    plt.ylim(ymin=0, ymax=1023)
+    plt.plot(np.arange(start=0, stop=data_len[row], step=1), ccd_data[row])
+    plt.show()
+    plt.pause(0.0001)
+    input("Press Enter to continue")
+
+        
+
+            

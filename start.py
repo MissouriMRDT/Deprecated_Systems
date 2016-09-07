bb57525b2756c051afda98cd7217f3a0b7cdc4ad
John Maruska <jwmbq6@mst.edu>
2016-09-07 10:19:14 -0500

    Add tab1 graph area. Tested with random plot.

--- start.py
+++ start.py
@@ -1,14 +1,48 @@
 import sys
-from PyQt4 import QtCore, QtGui
-from ui_untitled import Ui_MainWindow
+from PyQt4 import QtCore
+from PyQt4 import QtGui
+from matplotlib.backends.backend_qt4agg import FigureCanvasQTAgg as FigureCanvas
+from matplotlib.backends.backend_qt4agg import NavigationToolbar2QT as NavigationToolbar
+import matplotlib.pyplot as plt
+import random
+
+from ui_mainwindow import Ui_MainWindow
 
 class StartQT4(QtGui.QMainWindow):
     def __init__(self, parent=None):
         QtGui.QWidget.__init__(self, parent)
+
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
-        
-        
+
+        self.tempGraphFigure = plt.figure(1)
+        # create canvas
+        self.tempGraphCanvas = FigureCanvas(self.tempGraphFigure)
+        # create toolbar
+        self.tempGraphToolbar = NavigationToolbar(self.tempGraphCanvas, self)
+        # layout the graph area
+        tempGraphLayout = QtGui.QVBoxLayout()
+        tempGraphLayout.addWidget(self.tempGraphCanvas)
+        tempGraphLayout.addWidget(self.tempGraphToolbar)
+
+        # TODO: delet this
+        self.button = QtGui.QPushButton('Plot')
+        self.button.clicked.connect(self.plot)
+        tempGraphLayout.addWidget(self.button)
+
+        # add widget into horizontalLayout_3
+        self.ui.horizontalLayout_3.addLayout(tempGraphLayout)
+
+        # self.plainTextEdit_4 = QtGui.QPlainTextEdit(self.ui.tab_4)
+        # self.plainTextEdit_4.setObjectName(_fromUtf8("plainTextEdit_4"))
+
+    def plot(self):
+        data = [random.random() for i in range(10)]
+        ax = self.tempGraphFigure.add_subplot(111)
+        ax.hold(False)
+        ax.plot(data, '*-')
+        self.tempGraphCanvas.draw()
+
 if __name__ == "__main__":
     app = QtGui.QApplication(sys.argv)
     myapp = StartQT4()

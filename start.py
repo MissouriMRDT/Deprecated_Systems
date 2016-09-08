9e4acbdf64d5582c9f1578b00d0fe5a8bc5b51d1
John Maruska <jwmbq6@mst.edu>
2016-09-07 19:04:54 -0500

    Fix margin error. Bandaid fix.

--- start.py
+++ start.py
@@ -1,13 +1,22 @@
 import sys
 from PyQt4 import QtCore, QtGui
-from ui_untitled import Ui_MainWindow
+from ui_mainwindow import Ui_MainWindow
 
 class StartQT4(QtGui.QMainWindow):
     def __init__(self, parent=None):
         QtGui.QWidget.__init__(self, parent)
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
-        
+
+    def fix_margins(self):
+        """
+            Spacing doesn't go through with pyuic4 utility for some reason.
+            This block just shifts to how it should be done in ui_mainwindow.py
+        """
+        self.ui.verticalLayout_2.setContentsMargins(0, 11, 11, 11)
+        self.ui.verticalLayout_2.setSpacing(0)
+        self.ui.inputFrame.setContentsMargins(25, 11, 11, 0)
+
         
 if __name__ == "__main__":
     app = QtGui.QApplication(sys.argv)

fed24b301c2c992cfc2ebbe8abee4ec4e79d80f0
John Maruska <jwmbq6@mst.edu>
2016-10-08 15:42:19 -0500

    Add file-input parsing for CSV files.
    File-input code is currently inside the start.py file. Efforts should be
    made later to separate more specialized code away from the start file;
    i.e. move the file parsing code into the custom file.

--- science_station/start.py
+++ science_station/start.py
@@ -1,12 +1,17 @@
+import csv
+import dateutil.parser
 import sys
 from PyQt4 import QtGui, QtCore
 import customWidgets
 
 
-# TO DO: functions - parse file name, parse file matching name if exists, plot data
+# TODO: functions - parse file name, parse file matching name if exists, plot data
 
 class StartQT4(QtGui.QMainWindow):
     def __init__(self, parent=None):
+        self.current_file = ""
+        self.ds = DataStore()
+
         QtGui.QWidget.__init__(self, parent)
         self.resize(946, 542)
         self.setSizeIncrement(QtCore.QSize(1, 1))
@@ -53,11 +58,65 @@ class StartQT4(QtGui.QMainWindow):
         self.digMainLayout.addLayout(self.inputFrame)
         self.displayFrame.addWidget(self.graphTabs)
         self.digMainLayout.addLayout(self.displayFrame)
+
+        self.connect(self.fileInput, QtCore.SIGNAL("returnPressed()"), self.enterfile)
+
         self.setCentralWidget(self.centralWidget)
 
+    def enterfile(self):
+        filename = self.fileInput.text()
+        if filename != self.current_file:
+            if filename.lower().endswith('.csv'):
+                self.parsecsv(filename)
+                self.current_file = filename
+            else:
+                print("Unsupported file type. Please input a .csv file.")
+        else:
+            print("File requested is already entered.")
 
-if __name__ == "__main__":
+    def parsecsv(self, csv_name):
+        with open(csv_name, 'r') as csvfile:
+            csv.reader(csvfile, delimiter=' ')
+            # check header for Spectrometer or Temp/Humid readings.
+            # Assuming Temp/Humid
+            for row in csvfile.readlines():
+                datestamp, sensor, raw_data = row.split(' ')
+                if sensor == "Temp1":
+                    self.ds.temp1.append((dateutil.parser.parse(datestamp), raw_data))
+                elif sensor == "Temp2":
+                    self.ds.temp2.append((dateutil.parser.parse(datestamp), raw_data))
+                elif sensor == "Temp3":
+                    self.ds.temp3.append((dateutil.parser.parse(datestamp), raw_data))
+                elif sensor == "Temp4":
+                    self.ds.temp4.append((dateutil.parser.parse(datestamp), raw_data))
+                elif sensor == "Humid1":
+                    self.ds.humid1.append((dateutil.parser.parse(datestamp), raw_data))
+                elif sensor == "Humid2":
+                    self.ds.humid2.append((dateutil.parser.parse(datestamp), raw_data))
+                elif sensor == "Humid3":
+                    self.ds.humid3.append((dateutil.parser.parse(datestamp), raw_data))
+                elif sensor == "Humid4":
+                    self.ds.humid4.append((dateutil.parser.parse(datestamp), raw_data))
+        self.ds.totalentries()
+
+
+class DataStore:
+    def __init__(self):
+        self.temp1 = []
+        self.temp2 = []
+        self.temp3 = []
+        self.temp4 = []
+        self.humid1 = []
+        self.humid2 = []
+        self.humid3 = []
+        self.humid4 = []
+
+
+def main():
     app = QtGui.QApplication(sys.argv)
     myapp = StartQT4()
     myapp.show()
     sys.exit(app.exec_())
+
+if __name__ == "__main__":
+    main()

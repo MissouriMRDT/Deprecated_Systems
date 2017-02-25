8e1b55fd7c7736333cab0f613e68d56b0a6128b7
JimmyHaviland <wjhcp6@mst.edu>
2017-02-25 17:34:51 -0600

    Updated main function to integrate RED and all components

--- ScienceIntegrated/science.ino/science.h
+++ ScienceIntegrated/science.ino/science.h
@@ -17,8 +17,26 @@ const uint8_t IP_ADDRESS [4] = {192, 168, 1, 135};
 
 //functions
 
-void initialize();
 
-void operate_flap(const int16_t flap);
-void turnLaserOn();
-void turnLaserOff();
+void initialize();
+void spectrometer();
+void partial_spec(const uint16_t data);
+
+
+void turnOnLaser();
+void turnOffLaser();
+void openFlap();
+void closeFlap();
+void rotateCarousel(const uint16_t pos);//Rotates carosel to position 0-4 from current position
+void spectroMotorOn(const bool dir);//Direction is either positive or negative (0 or 1)
+void spectroMotorOff();
+float instantSoilTemp();//Returns one Soil Temperature reading
+float instantAirTemp();//Returns one Air Termperature reading
+float instantUV();//Return one UV intensisty reading
+//int averageAnalogRead(int pinToRead);//Unused function for old version of UV read
+float mapfloat(float x, float in_min, float in_max, float out_min, float out_max);//Maps the float value for UV intensity
+float instantPressure();//Returns one pressure reading
+int readPhotoDiode1();
+int readPhotoDiode2();
+
+
--- ScienceIntegrated/science.ino/science.ino.ino
+++ ScienceIntegrated/science.ino/science.ino.ino
@@ -1,7 +1,12 @@
+//Programmers: Chris Dutcher & Jimmy Haviland
+//Science board Main program
+
 #include "science.h"
 #include "RoveEthernet.h"
 #include <SPI.h>
 #include "Servo.h"
+#include <Wire.h>
+#include "BMP085_t.h"
 
 //Variable declaration
 CommandResult result;
@@ -10,6 +15,7 @@ size_t commandSize;
 int16_t commandData;
 uint32_t watchdogTimer_us = 0;
 Servo flap, carousel;
+BMP085<0> PSensor;
 
 void setup() {}
 
@@ -26,6 +32,8 @@ void loop() {
     commandSize = 0;
     commandId = 0;
     commandData = 0;
+    bool sensor_enable[7] = {1,1,1,1,1,1,1};//Determines which sensors send data back
+
 
     roveComm_GetMsg(&commandId, &commandSize, &commandData);
 
@@ -37,17 +45,53 @@ void loop() {
       Serial.println(commandData);
       Serial.println(commandSize);
       Serial.println("");
-      const int response = 0;
-      roveComm_SendMsg(commandId, commandSize, &response); //dataID, size, data
 
-//       if(commandId == 0x711)
-//        flap(commandData)
-
-        //if(sucsess...)
-        
+       if(commandId == 0x720)
+         sensor_enable[0]=commandId;
+       else if(commandId == 0x721)
+         sensor_enable[1]=commandId;
+       else if(commandId == 0x722)
+         sensor_enable[2]=commandId;
+       else if(commandId == 0x723)
+         sensor_enable[3]=commandId;
+       else if(commandId == 0x728)
+         sensor_enable[4]=commandId;
+       else if(commandId == 0x729)
+         sensor_enable[5]=commandId;
+       else if(commandId == 0x72A)
+         sensor_enable[6]=commandId;
+       else if(commandId == 0x711)//Carosel
+         rotateCarousel(commandData);
+       else if(commandId == 0x710)//Spectrometer sub-routine
+         spectrometer();
+       else if(commandId == 0x72B)//
+         partial_spec(commandData);
+       
+       //check for sensors, and then send through rovecomm if applicable
+       if(sensor_enable[0])
+         //AirTemp
+         delay(1);
+       else if(sensor_enable[1])
+         //AirHumidity
+         delay(1);
+       else if(sensor_enable[2])
+         //Soil Temp
+         delay(1);
+       else if(sensor_enable[3])
+         //Soil Humidity
+         delay(1);
+         else if(sensor_enable[4])
+         //Methane
+         delay(1);
+       else if(sensor_enable[5])
+         //UV Intensity
+         delay(1);
+       else if(sensor_enable[6])
+         //Pressure
+         delay(1);      
         
     }
-    else
+    else//No message
     {
       uint8_t microsecondDelay = 10;
       delayMicroseconds(microsecondDelay);
@@ -60,7 +104,7 @@ void loop() {
         watchdogTimer_us = 0;
       }
 
-      //if(checkOvercurrent())
+      //if(checkOvercurrent())?
     }
     
   }
@@ -76,6 +120,7 @@ void initialize()
   pinMode(PD_1, INPUT);//photodiode2
   flap.attach(PM_5);
   carousel.attach(PM_4);
+  PSensor.begin();//Initalize pressure Sensor
   /************************************
   * Spectrometer motor Initiaslization
   /***********************************/
@@ -89,14 +134,46 @@ void initialize()
   Serial.println("Initialized!");
 }
 
-void operate_flap(const int16_t data)
+
+
+void spectrometer()//Spec sub-routine
 {
-  if(data==1)
-    Serial.println("Closing Flap");
-  else if(data==2)
-    Serial.println("Opening Flap");
-   else
-    Serial.println("Neither");
+  //Motor on
+  //5 sec
+  //laser on
+  //start reading pd
+  //30 sec
+  //laser off
+  //motor off
+  //delay 2
+  //motor reverse
+  //stop reading 
+  //delay 35
+  //motor off
+}
+
+void partial_spec(const uint16_t data)
+{
+    if(data==0)
+    openFlap();
+    else if(data==1)
+    closeFlap();
+    else if(data==2)
+    turnOnLaser();
+    else if(data==3)
+    turnOffLaser();
+    else if(data==4)
+    spectroMotorOn(0);
+    else if(data==5)
+    spectroMotorOn(1);
+    else if(data==6)
+    spectroMotorOff();
+    else if(data==7)
+    //Read PD1 and sends back result
+    delay(1000);
+    else if(data==8)
+    //Read PD2 and sends back result
+    delay(1000);
 }
 
 void turnOnLaser()
@@ -125,13 +202,12 @@ void closeFlap()
   flap.write(0);
 }
 
-void rotateCarousel(int pos)//need to discuss best implementation of this
+void rotateCarousel(const uint16_t pos)//need to discuss best implementation of this
 {
-  pos--;
   carousel.write(180/4 * pos);  //cache positions must be tweeked here.
 }
 
-void spectroMotorOn(bool dir)
+void spectroMotorOn(const bool dir)
 {
   digitalWrite(PM_6, HIGH);//nsleep
   if (dir)
@@ -146,5 +222,80 @@ void spectroMotorOff()
 {
   digitalWrite(PP_3, LOW);//en
   digitalWrite(PM_6, LOW);//nsleep
+}
+
+float instantSoilTemp()
+{
+  const int analogRes = 4095;
+  float voltage = (analogRead(PB_5) * 3.24 / analogRes);
+  float degC = (voltage - 0.5) * 100.0;
+  return degC;
+}
+
+
+float instantAirTemp()
+{
+  const int analogRes = 4095;
+  float voltage = (analogRead(PK_2) * 3.24 / analogRes);
+  float degC = (voltage - 0.5) * 100.0;
+  return degC;
+}
+
+float instantUV()
+{
+ //Enable pin here
+  float uvInten = mapfloat(analogRead(PK_0), .99, 2.8, 0, 15);
+  return uvInten;
+
+
+  /*  //Old unused version of UV reading
+  int uvLevel = averageAnalogRead();         //Needs correct PIN   PK_0
+  int refLevel = averageAnalogRead();      //Needs correct PIN
+
+  //Use the 3.3V power pin as a reference to get a very accurate output value from sensor
+  float outputVoltage = 3.3 / refLevel * uvLevel;
+  float uvIntensity = mapfloat(outputVoltage, 0.99, 2.8, 0.0, 15.0); //Convert the voltage to a UV intensity level
+  return uvIntensity;
+  */
+}
+
+/*  //Old function for UV reading
+//Takes an average of readings on a given pin
+//Returns the average
+int averageAnalogRead(int pinToRead)
+{
+  byte numberOfReadings = 8;
+  unsigned int runningValue = 0; 
+
+  for(int x = 0 ; x < numberOfReadings ; x++)
+    runningValue += analogRead(pinToRead);
+  runningValue /= numberOfReadings;
+
+  return(runningValue);  
+}
+*/
+
+//The Arduino Map function but for floats
+//From: http://forum.arduino.cc/index.php?topic=3922.0
+float mapfloat(float x, float in_min, float in_max, float out_min, float out_max)
+{
+  return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
+
+float instantPressure()
+{
+  PSensor.refresh();
+  PSensor.calculate();
+  return PSensor.pressure;
+}
+
+int readPhotoDiode1()
+{
+  return analogRead(PD_0);  
+}
+
+int readPhotoDiode2()
+{
+  return analogRead(PD_1);
 }
 

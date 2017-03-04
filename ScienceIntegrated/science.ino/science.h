28a4dd2015c7e7644c617972b8962e77f97a22e4
Christopher Dutcher <crdwyb@mst.edu>
2017-03-04 17:09:25 -0600

    Added more comments to improve readability
    Added comments for functions, control blocks, and the mail execution
    loop, Added TODO: coments

--- ScienceIntegrated/science.ino/science.h
+++ ScienceIntegrated/science.ino/science.h
@@ -11,24 +11,22 @@ typedef enum CommandResult
 } CommandResult;
 
 
-//var/pins
+//var or pins
 const uint32_t WATCHDOG_TIMEOUT_US = 2000000; //the amount of microseconds that should pass without getting a transmission from base station before the arm ceases moving for safety
 const uint8_t IP_ADDRESS [4] = {192, 168, 1, 135};
 
 //functions
 
-
-void initialize();
-void spectrometer();
-void partial_spec(const uint16_t data);
-
+void initialize();//All important pre-loop setup goes here
+void spectrometer();//Full spectrometer sub-routine
+void partial_spec(const uint16_t data);//(Currently unused) runs individual spectrometer parts
 
 void turnOnLaser();//Turns On the Spectrometer Laser
 void turnOffLaser();//Turns Off the Spectrometer Laser
 void openFlap();//Opens the Sample Cache Flap
 void closeFlap();//Closes the Sample Cache Flap
 void rotateCarousel(const uint16_t pos);//Rotates carosel to position 0-4 from current position given a position (0-4)
-void spectroMotorOff();
+void spectroMotorOff();//Turns off the spectrometer motor
 float instantSoilTemp();//Returns one Soil Temperature reading
 float instantAirTemp();//Returns one Air Termperature reading
 float instantUV();//Return one UV intensisty reading
@@ -41,6 +39,4 @@ void spectroMotorReverse();//Turns the spectrometer motor on, but in the reverse
 float instantAirHumidity();//Placeholder
 float instantSoilHumidity();//Placeholder
 float instantMethane();//Placeholder
-
-
-
+float instantPressure();//Placeholder
--- ScienceIntegrated/science.ino/science.ino.ino
+++ ScienceIntegrated/science.ino/science.ino.ino
@@ -1,44 +1,59 @@
 //Programmers: Chris Dutcher & Jimmy Haviland
-//Science board Main program
+// Febuary 24, 2017
+//Missouri S&T  Mars Rover Design Team
+//Science Board Main program
 
 #include "science.h"
 #include "RoveEthernet.h"
 #include <SPI.h>
 #include "Servo.h"
 #include <Wire.h>
-//#include "BMP085_t.h"
+//#include "BMP085_t.h"   This file is causing compilation problems
 
-//Variable declaration
+//Variable declaration:
+//Stores the result (if any) of commands received and executed
 CommandResult result;
+//Stores the value received from base station, of which command to execute
 uint16_t commandId;
+//Stores the size of the commandData received from base station
 size_t commandSize;
+//Stores the command arguement, sent by base station, to send through to the command
 int16_t commandData;
+//Stores the value of watchdog, which times out after not receiving a command from base station, to terminate any potential dangerous operations
 uint32_t watchdogTimer_us = 0;
+
+//Device objects
+//Energia standard servo device class from Servo.h
 Servo flap, carousel;
+//Pressure sensor object, currently uses problematic file
 //BMP085<0> PSensor;
 
+//All non-important pre-loop setup is done here
 void setup() {}
 
+
 void loop() {
-
-
-
-  //Initialize
+  //All important pre-loop setup is done here
   initialize();
 
   //Main execution loop
   while(1)
   {
+    //Resets the message to nothing
     commandSize = 0;
     commandId = 0;
     commandData = 0;
-    bool sensor_enable[7] = {1,1,1,1,1,1,1};//Determines which sensors send data back
 
+    
+    bool sensor_enable[7] = {1,1,1,1,1,1,1};//Determines which sensors send data back  TODO:Does this re-initialize the vars each time?
 
+    //Receives a command form base station and stores the message
     roveComm_GetMsg(&commandId, &commandSize, &commandData);
-
+    
+    //Checks the message for an actual command
     if(commandId != 0)
     {
+      //We have received a command, so watchdog is reset
       watchdogTimer_us = 0;
 
       Serial.println(commandId);
@@ -46,6 +61,7 @@ void loop() {
       Serial.println(commandSize);
       Serial.println("");
 
+       //Checks the value of the command, and if applicable, executes it
        if(commandId == 0x710)
        {
          if(commandData == 1)
@@ -86,21 +102,18 @@ void loop() {
            closeFlap();
          else if(commandData == 17)
            spectrometer();
-
-           
        }
        else if(commandId == 0x711)//Carousel
        {
-        if(commandData!=5)//We have 5 positions (1-5) and 6 commands (0-6)
+        if(commandData!=5)//We have 5 positions (1-5) and 6 commands (0-6), rotate the carousel if it is a valid position
           rotateCarousel(commandData);
        }
-       
-
     }
-    else //No message and send sensor data
+    else //No message was received and we send sensor data
     {
+       //Temporary sensor variable, future variable will be altered by sensor functions before being sent
        float sensorTestData = 100.0;
-       //check for sensors, and then send through rovecomm if applicable
+       //check for which sensors to record, and then send through rovecomm
        if(sensor_enable[0])//AirTemp
        {
         sensorTestData = 100.0;
@@ -137,23 +150,26 @@ void loop() {
          roveComm_SendMsg(0x72A, sizeof(106.0), &sensorTestData);
        }        
 
+      //Delay before we check for another command from base station
       uint8_t microsecondDelay = 10;
       delayMicroseconds(microsecondDelay);
-
       watchdogTimer_us += microsecondDelay;
-
-      if(watchdogTimer_us >= WATCHDOG_TIMEOUT_US) //if more than our timeout period has passed, then kill arm movement
+      //Once we have spend two seconds delaying for commands, we terminate potentially dangerous (to astronaughts or the rover) operations and keep them disabled until we receive a command again
+      if(watchdogTimer_us >= WATCHDOG_TIMEOUT_US)
       {
+        //End dangerous operations here
         Serial.println("Watch out dog!!!!!!");
         watchdogTimer_us = 0;
       }
 
-      //if(checkOvercurrent())?
-    }
+    }//End else
     
-  }
-}
+  }//End while
+}//End loop()
 
+//Functions:
+
+//All important pre-loop setup is done here outside of setup, for some reason
 void initialize()
 {
   roveComm_Begin(IP_ADDRESS[0], IP_ADDRESS[1], IP_ADDRESS[2], IP_ADDRESS[3]);
@@ -164,7 +180,7 @@ void initialize()
   pinMode(PD_1, INPUT);//photodiode2
   flap.attach(PM_5);
   carousel.attach(PM_4);
-  //PSensor.begin();//Initalize pressure Sensor
+  //PSensor.begin();//Initalize pressure Sensor (non-compiling)
   /************************************
   * Spectrometer motor Initiaslization
   /***********************************/
@@ -179,31 +195,23 @@ void initialize()
 }
 
 
-
-void spectrometer()//Spec sub-routine
+//Spectometer sub-routine TODO: roveComm_SendMsg(...) the data
+void spectrometer()
 {
-
    uint16_t photo1, photo2;
-   
    //turn on motor, run for 5s before continuing
    //direction is a bool! change true/false for direction
-   spectroMotorForward();
-
-
-   
+   spectroMotorForward();   
    //do nothing except leave motor on for 5s
    delay(5000);
-
    //turn on laser
-   turnOnLaser();
-   
+   turnOnLaser();   
    //laser is on, motor should still be going?
    //keep laser and motor on for 30s
    int timer = 0;
-
-    //loop takes analog readings 
-    //delays for 1/8 second
-    //should repeat until roughly 30s have passed.
+   //loop takes analog readings 
+   //delays for 1/8 second
+   //should repeat until roughly 30s have passed.
    while (timer <= 30000)
    {
     //read photo diodes
@@ -216,34 +224,25 @@ void spectrometer()//Spec sub-routine
     timer += 125;
     delay(125); 
    }
-
    //turn off laser
    turnOffLaser();
-
    //turn off motor
    spectroMotorOff();
-
    //delay couple seconds
    delay(2000);
-
    //return motor to start position
    //opposite direction than what was called earlier!
    spectroMotorReverse();
-
    //wait 35s for motor to return
    delay(35000);
-
    //stop motor again
    spectroMotorOff();
-
    //wait 10 seconds before repeating the loop
-   delay(10000);
-  
-   
-   
-  
+   delay(10000); 
+   return;
 }
 
+//Given a specific part command, runs individual parts of the spectrometer (not currently used)
 void partial_spec(const uint16_t data)
 {
     if(data==0)
@@ -261,51 +260,62 @@ void partial_spec(const uint16_t data)
     else if(data==6)
     spectroMotorOff();
     else if(data==7)
-    //Read PD1 and sends back result
-    delay(1000);
+    readPhotoDiode1();
     else if(data==8)
-    //Read PD2 and sends back result
-    delay(1000);
+    readPhotoDiode1();
+    return;
 }
 
+//Turns on the spectrometer laser
 void turnOnLaser()
 {
   //turn on laser by setting pin to High
   digitalWrite(PQ_3, HIGH);//laser
   digitalWrite(PF_0, HIGH);//LED
+  return;
 }
 
+//Turns off the spectormeter laser
 void turnOffLaser()
 {
   //turn off laser by setting pin to low
   digitalWrite(PQ_3, LOW);//laser
   digitalWrite(PF_0, LOW);//led
+  return;
 }
 
+//Opens the sample cache cover flap
 void openFlap()
 {
   //does flap open at 180 degrees or 0?
   flap.write(170);
+  return;
 }
 
+//Closes the sample cache cover flap
 void closeFlap()
 {
   //again, does flap close at 180 degrees or 0?
   flap.write(0);
+  return;
 }
 
-void rotateCarousel(const uint16_t pos)//need to discuss best implementation of this
+//Rotates the sample cache carousel to the given position
+void rotateCarousel(const uint16_t pos)
 {
-  carousel.write(180/4 * pos);  //cache positions must be tweeked here.
+  carousel.write(180/4 * pos);  // TODO: cache positions must be tweeked here.
+  return;
 }
 
-
+//Turns off the spectrometer motor
 void spectroMotorOff()
 {
   digitalWrite(PP_3, LOW);//en
   digitalWrite(PM_6, LOW);//nsleep
+  return;
 }
 
+//Returns one soil temperature reading
 float instantSoilTemp()
 {
   const int analogRes = 4095;
@@ -314,7 +324,7 @@ float instantSoilTemp()
   return degC;
 }
 
-
+//Returns one air temperature reading
 float instantAirTemp()
 {
   const int analogRes = 4095;
@@ -323,6 +333,7 @@ float instantAirTemp()
   return degC;
 }
 
+//Returns on UV intensity reading
 float instantUV()
 {
  //Enable pin here
@@ -330,62 +341,68 @@ float instantUV()
   return uvInten;
 }
 
-
-//The Arduino Map function but for floats
+//The Arduino Map function but for floats (used in UV intensity)
 //From: http://forum.arduino.cc/index.php?topic=3922.0
 float mapfloat(float x, float in_min, float in_max, float out_min, float out_max)
 {
   return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 }
 
-/*
-float instantPressure()
-{
-  PSensor.refresh();
-  PSensor.calculate();
-  return PSensor.pressure;
-}
-*/
-
+//Returns one PhotoDiode 1 reading
 int readPhotoDiode1()
 {
   return analogRead(PD_0);  
 }
 
+//Returns one PhotoDiode 2 reading
 int readPhotoDiode2()
 {
   return analogRead(PD_1);
 }
 
+//Turns on the spectrometer motor on in the forward direction (the direction it will go when reading the photo-diodes)
 void spectroMotorForward()
 {
    digitalWrite(PQ_1, LOW);//phase, low = forward
    digitalWrite(PP_3, HIGH);//enable
    digitalWrite(PM_6, HIGH);//nSleep
+   return;
 }
 
+//Turns the spectrometer motor on in the backwards direciton (when the spectrometer resets)
 void spectroMotorReverse()
 {
    digitalWrite(PQ_1, HIGH);
    digitalWrite(PP_3, HIGH);
    digitalWrite(PM_6, HIGH);
+   return;
 }
 
+//Returns one reading of the air humidity TODO: Insert actual sensor code
 float instantAirHumidity()
 {
   float holderOfPlace = 0.0;
   return holderOfPlace;  
 }
 
+//Returns one reading of the soil humidity TODO: Insert actual sensor code
 float instantSoilHumidity()
 {
   float holderOfPlace = 0.0;
   return holderOfPlace;    
 }
 
+//Returns one reading of methane TODO: Insert actual sensor code
 float instantMethane()
 {
   float holderOfPlace = 0.0;
   return holderOfPlace;  
+}
+
+//Returns one air pressure reading TODO: Insert actual sensor code
+float instantPressure()
+{
+  float holderOfPlace = 0.0;
+  return holderOfPlace;
 }
 

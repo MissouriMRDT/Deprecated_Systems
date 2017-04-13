a8005fbd243cdc0924060b1d641892d4b2a719d8
JimmyHaviland <wjhcp6@mst.edu>
2017-04-12 20:55:36 -0500

    add this file
    add this file

--- Sensor Stuff/TMP36 Sketch/TMP36_Weather/TMP36_Weather.ino
+++ Sensor Stuff/TMP36 Sketch/TMP36_Weather/TMP36_Weather.ino
@@ -1,4 +1,5 @@
 //TMP36 Pin Variables
+int sensorPin = PD_1; //the analog pin the TMP36's Vout (sense) pin is connected to
                         //the resolution is 10 mV / degree centigrade with a
                         //500 mV offset to allow for negative temperatures
  

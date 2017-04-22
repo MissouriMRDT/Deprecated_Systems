21ecaf3843b3c1569498ab30e4e7e105000f9595
kbrinker12 <krbzhb@mst.edu>
2017-04-22 10:44:18 -0500

    Updated laser pin

--- TestSketches/LaserPhotoTest/LaserPhotoTest.ino
+++ TestSketches/LaserPhotoTest/LaserPhotoTest.ino
@@ -1,12 +1,12 @@
   //declare integers for analog input, will be between 0 and 1023
   uint16_t analog1, analog2;
 
-  int laserPin = PD_4;
+  int laserPin = 46;
   
 
 void setup() {
   // put your setup code here, to run once:
-  pinMode(PD_4, OUTPUT);//laser
+  pinMode(laserPin, OUTPUT);//laser
   pinMode(PF_0, OUTPUT);//LED
   pinMode(PD_2, INPUT);//photo 1
   pinMode(PD_3, INPUT);//photo 2
@@ -18,18 +18,18 @@ void loop() {
   // put your main code here, to run repeatedly: 
 
   
-  //read analog data
-  analog1 = analogRead(PD_2);
-  //some command here to send this value to base station
-  analog2 = analogRead(PD_3);
-
-  Serial.print("analog1 = ");
-  Serial.print(analog1);
-  Serial.print("\n");
-
-  Serial.print("analog2 = ");
-  Serial.print(analog2);
-  Serial.print("\n");  
+//  //read analog data
+//  analog1 = analogRead(PD_2);
+//  //some command here to send this value to base station
+//  analog2 = analogRead(PD_3);
+//
+//  Serial.print("analog1 = ");
+//  Serial.print(analog1);
+//  Serial.print("\n");
+//
+//  Serial.print("analog2 = ");
+//  Serial.print(analog2);
+//  Serial.print("\n");  
   
 
   //turn on laser function
@@ -37,7 +37,7 @@ void loop() {
   turnOnLaser();
   
 
-  delay(10000);
+  delay(20000);
 
   //turn off laser function
   turnOffLaser();
@@ -52,13 +52,13 @@ void loop() {
 void turnOnLaser()
 {
   //turn on laser by setting pin to High
-  digitalWrite(PD_4, HIGH);//laser
+  digitalWrite(laserPin, HIGH);//laser
   digitalWrite(PF_0, HIGH);//LED
 }
 
 void turnOffLaser()
 {
   //turn off laser by setting pin to low
-  digitalWrite(PD_4, LOW);//laser
+  digitalWrite(laserPin, LOW);//laser
   digitalWrite(PF_0, LOW);//led
 }
